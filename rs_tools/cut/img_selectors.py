ee010f0453d2bdd793077386ce44b6f5db326e1b
Rustam Antia <rustam.antia@gmail.com>
2021-05-06 14:48:15 +0000

    inital commit for refactoring of cutting methods

---
+++ rs_tools/cut/img_selectors.py
@@ -0,0 +1,58 @@
+"""
+Callable classes for selecting a sublist from a list of images used by cutting functions. 
+"""
+
+from typing import List
+import random
+from geopandas import GeoDataFrame
+from collections.abc import Callable
+from abc import abstractmethod
+from rs_tools import ImgPolygonAssociator
+
+
+class ImgSelector(Callable):
+    """
+    Abstract base class for selecting from a list of images. Subclasses are used by create_or_update_dataset_from_iter_over_polygons.
+
+    Subclasses should implement a __call__method that has the arguments and behavior given below.
+    """
+
+    @abstractmethod
+    def __call__(self, img_names_list: List[str], 
+                new_polygons_df: GeoDataFrame, 
+                source_assoc: ImgPolygonAssociator) -> List[str]:
+        """
+        Args:
+            img_names_list (List[str]): list of images to be selected from
+            new_polygons_df (GeoDataFrame): polygons_df of new associator/dataset to be created for the new images created by the cutting function
+            source_assoc (ImgPolygonAssociator): associator of source dataset that new images are being cut out from 
+
+        Returns:
+            List[str]: sublist of img_names_list
+        """
+        
+        raise NotImplementedError
+
+
+class RandomImgSelector(ImgSelector):
+    """
+    ImgSelector that randomly selects a single image from a list of images.
+    """
+    def __init__(self):
+        return
+
+    def __call__(self, img_names_list: List[str], 
+                new_polygons_df: GeoDataFrame, 
+                source_assoc: ImgPolygonAssociator) -> List[str]:
+        """
+        Args:
+            img_names_list (List[str]): list of images to be selected from
+            new_polygons_df (GeoDataFrame): polygons_df of new associator/dataset to be created for the new images created by the cutting function
+            source_assoc (ImgPolygonAssociator): associator of source dataset that new images are being cut out from 
+
+        Returns:
+            List[str]: sublist of img_names_list
+        """
+        return [random.choice(img_names_list)] if img_names_list != [] else []    
+
+random_img_selector = RandomImgSelector()
