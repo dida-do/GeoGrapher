6eb579bce2bfe83a1cedb8e948648d04d6099bb7
Rustam Antia <rustam.antia@dida.do>
2021-08-18 13:41:41 +0000

    added img/polygon filter predicates that filter by predicate of row of dataframe

--- rs_tools/cut/img_filter_predicates.py
+++ rs_tools/cut/img_filter_predicates.py
@@ -1,4 +1,6 @@
-from geopandas import GeoDataFrame
+from typing import Any, Callable, Union
+from geopandas import GeoDataFrame, GeoSeries
+from pandas import Series
 from collections.abc import Callable
 from abc import abstractmethod
 from rs_tools import ImgPolygonAssociator
@@ -40,3 +42,38 @@ class AlwaysTrue(ImgFilterPredicate):
                     source_assoc: ImgPolygonAssociator) -> bool:
         """Return True"""
         return True
+
+
+class ImgFilterRowCondition(ImgFilterPredicate):
+    """
+    Simple ImgFilter that applies a given predicate to the information in the row corresponding to the image name in question. 
+    """
+
+    def __init__(self, 
+        row_series_predicate : Callable[[Union[GeoSeries, Series]], bool]
+        ) -> None:
+        """
+        Args:
+            row_series_predicate (Callable[[Union[GeoSeries, Series]], bool]): predicate to apply to the row corresponding to an image (i.e. source_assoc.imgs_df.loc[img_name])
+        """
+        
+        super().__init__()
+        self.row_series_predicate = row_series_predicate
+
+    def __call__(self, 
+            img_name: str, 
+            source_assoc: ImgPolygonAssociator
+            ) -> bool:
+        """
+        Apply self.row_series_predicate to source_assoc.imgs_df[img_name]
+
+        Args:
+            img_name (str): image name
+            source_assoc (ImgPolygonAssociator): source associator
+
+        Returns:
+            bool: result of aplying self.row_series_predicate to source_assoc.imgs_df[img_name]
+        """
+
+        row_series : Union[GeoSeries, Series] = source_assoc.imgs_df.loc[img_name]
+        return self.row_series_predicate(row_series) 
