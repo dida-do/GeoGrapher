8f63769890114036f76f87f81e717cd6dacef06e
Rustam Antia <rustam.antia@dida.do>
2021-09-03 16:31:38 +0000

    cut: changed __init__ to account for renaming of files

--- rs_tools/cut/__init__.py
+++ rs_tools/cut/__init__.py
@@ -1,14 +1,2 @@
-"""
-Functions to create or update datasets of GeoTiffs by cutting images by iterating over polygons.
-
-new_dataset_one_small_img_for_each_polygon cuts square images surrounding each polygon. 
-update_dataset_one_small_img_for_each_polygon updates a dataset created with 
-
-new_dataset_one_small_img_for_each_polygon.
-
-create_or_update_dataset_from_iter_over_polygons is a very general function to cut datasets by iterating over images.
-"""
-
-from rs_tools.cut.cut_dataset_iter_over_polygons import new_tif_dataset_small_imgs_for_each_polygon
-from rs_tools.cut.cut_dataset_iter_over_polygons import update_tif_dataset_small_imgs_for_each_polygon
-from rs_tools.cut.cut_dataset_iter_over_polygons import create_or_update_tif_dataset_from_iter_over_polygons
+from rs_tools.cut.cut_dataset_imgs_around_every_polygon import cut_dataset_imgs_around_every_polygon, update_dataset_imgs_around_every_polygon
+from rs_tools.cut.cut_dataset_every_img_to_grid import cut_dataset_every_img_to_grid, update_dataset_every_img_to_grid
