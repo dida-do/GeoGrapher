0ebb117f43dc3401ea888c0ec77a3a99fa7c0371
Rustam Antia <rustam.antia@gmail.com>
2021-09-03 16:45:49 +0000

    img_filter_predicate: changed signature (can depend on information in both associators)

--- rs_tools/cut/img_filter_predicates.py
+++ rs_tools/cut/img_filter_predicates.py
@@ -15,16 +15,23 @@ class ImgFilterPredicate(Callable):
 
     @abstractmethod
     def __call__(self, 
-                img_name: str, 
-                source_assoc: ImgPolygonAssociator
-                ) -> bool:
+            img_name : str, 
+            target_assoc : ImgPolygonAssociator, 
+            new_img_dict : dict, 
+            source_assoc : ImgPolygonAssociator
+            ) -> bool:
         """
         Args:
             img_name (str): img identifier
+            target_assoc (ImgPolygonAssociator): associator of target dataset. 
+            new_imgs_dict (dict): dict with keys index or column names of target_assoc.imgs_df and values lists of entries correspondong to images 
             source_assoc (ImgPolygonAssociator): associator of source dataset that new images are being cut out from 
 
         Returns:
             bool: True should mean image is to be kept, False that it is to be filtered out
+
+        Note: 
+            The new_imgs_dict should be viewed as part of the target associator. See polygon_filter_predicates.py for an explanation. 
         """
         raise NotImplementedError
 
@@ -38,18 +45,22 @@ class AlwaysTrue(ImgFilterPredicate):
         super().__init__()
 
     def __call__(self, 
-                    img_name: str, 
-                    source_assoc: ImgPolygonAssociator) -> bool:
+            img_name : str, 
+            target_assoc : ImgPolygonAssociator, 
+            new_img_dict : dict, 
+            source_assoc : ImgPolygonAssociator
+            ) -> bool:
         """Return True"""
         return True
 
 
 class ImgFilterRowCondition(ImgFilterPredicate):
     """
-    Simple ImgFilter that applies a given predicate to the information in the row corresponding to the image name in question. 
+    Simple ImgFilter that applies a given predicate to the row in source_assoc.imgs_df corresponding to the image name in question. 
     """
 
     def __init__(self, 
+
         row_series_predicate : Callable[[Union[GeoSeries, Series]], bool]
         ) -> None:
         """
@@ -61,14 +72,19 @@ class ImgFilterRowCondition(ImgFilterPredicate):
         self.row_series_predicate = row_series_predicate
 
     def __call__(self, 
-            img_name: str, 
-            source_assoc: ImgPolygonAssociator
+            img_name : str, 
+            target_assoc : ImgPolygonAssociator, 
+            new_img_dict : dict, 
+            source_assoc : ImgPolygonAssociator
             ) -> bool:
         """
         Apply self.row_series_predicate to source_assoc.imgs_df[img_name]
 
         Args:
+        
             img_name (str): image name
+            target_assoc (ImgPolygonAssociator): associator of target dataset. 
+            new_imgs_dict (dict): dict with keys index or column names of target_assoc.imgs_df and values lists of entries correspondong to images 
             source_assoc (ImgPolygonAssociator): source associator
 
         Returns:
