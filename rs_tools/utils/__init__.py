06cc4b75449cb4eef863b08bc3bc857504738148
Rustam Antia <rustam.antia@gmail.com>
2021-09-09 17:09:42 +0000

    modified __init__s

--- rs_tools/utils/__init__.py
+++ rs_tools/utils/__init__.py
@@ -5,5 +5,4 @@ convert_assoc_dataset_tif2npy converts a dataset of GeoTiffs to .npys.
 imgs_df_from_tif_dir generates an imgs_df GeoDataFrame from a directory of GeoTiffs.
 """
 
-from rs_tools.utils.assoc_tif2npy import convert_assoc_dataset_tif2npy
 from rs_tools.utils.imgs_df_from_tif_dir import imgs_df_from_tif_dir
