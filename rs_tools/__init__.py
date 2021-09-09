222e04aad5f79b76e2b71e7b5e270742f2d705d3
Rustam Antia <rustam.antia@gmail.com>
2021-09-09 17:09:42 +0000

    modified __init__s

--- rs_tools/__init__.py
+++ rs_tools/__init__.py
@@ -1,5 +1,2 @@
 from rs_tools.img_polygon_associator import ImgPolygonAssociator
-from rs_tools.utils.assoc_tif2npy import convert_assoc_dataset_tif2npy
-from rs_tools.utils.imgs_df_from_tif_dir import imgs_df_from_tif_dir
-from rs_tools.utils.associator_utils import empty_polygons_df
-from rs_tools.utils.associator_utils import empty_imgs_df
+
