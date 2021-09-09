d34bca9ada9f38a90581801f173d6267a1167e69
Rustam Antia <rustam.antia@dida.do>
2021-09-09 16:35:05 +0000

    associator_utils.py: moved empty_assoc_same_format_as to associator class. formatting

--- rs_tools/utils/associator_utils.py
+++ rs_tools/utils/associator_utils.py
@@ -6,19 +6,19 @@ from pathlib import Path
 from typing import Dict, Union, Optional, TYPE_CHECKING
 
 import pandas as pd
-import geopandas as gpd
+from geopandas import GeoDataFrame, GeoSeries
 
 from rs_tools.global_constants import STANDARD_CRS_EPSG_CODE
 if TYPE_CHECKING:
-    from rs_tools.img_polygon_associator import ImgPolygonAssociatorClass
-
-
+    from rs_tools.img_polygon_associator import ImgPolygonAssociator
 from rs_tools.graph.bipartite_graph import BipartiteGraph, empty_bipartite_graph
 
 
-def empty_gdf(df_index_name: str,
-                df_cols_and_index_types: Dict[str, str],
-                crs_epsg_code: int=STANDARD_CRS_EPSG_CODE) -> gpd.GeoDataFrame:
+def empty_gdf(
+        df_index_name : str,
+        df_cols_and_index_types : Dict[str, str],
+        crs_epsg_code : int=STANDARD_CRS_EPSG_CODE
+        ) -> GeoDataFrame:
     """Return a empty GeoDataFrame with specified index and column names and types and crs.
 
     :param df_index_name: name of the index of the new empty GeoDataFrame
@@ -28,18 +28,21 @@ def empty_gdf(df_index_name: str,
     :return: new_empty_df: the empty polygons_df GeoDataFrame.
     """
 
-    new_empty_gdf_dict = {'geometry': gpd.GeoSeries([]),
+    new_empty_gdf_dict = {'geometry': GeoSeries([]),
                                 **{index_or_col_name: pd.Series([], dtype=index_or_col_type) 
                                     for index_or_col_name, index_or_col_type in df_cols_and_index_types.items()
                                         if index_or_col_name != 'geometry'}}
-    new_empty_gdf = gpd.GeoDataFrame(new_empty_gdf_dict, crs=f"EPSG:{crs_epsg_code}")
+    new_empty_gdf = GeoDataFrame(new_empty_gdf_dict, crs=f"EPSG:{crs_epsg_code}")
     new_empty_gdf.set_index(df_index_name, inplace=True)
+    
     return new_empty_gdf
 
 
-def empty_imgs_df(imgs_df_index_name: str,
-                    imgs_df_cols_and_index_types: Dict[str, str],
-                    crs_epsg_code: int=STANDARD_CRS_EPSG_CODE) -> gpd.GeoDataFrame:
+def empty_imgs_df(
+        imgs_df_index_name : str,
+        imgs_df_cols_and_index_types : Dict[str, str],
+        crs_epsg_code : int=STANDARD_CRS_EPSG_CODE
+        ) -> GeoDataFrame:
     """
     Return a generic empty imgs_df GeoDataFrame conforming to the ImgPolygonAssociator format.
     
@@ -53,9 +56,11 @@ def empty_imgs_df(imgs_df_index_name: str,
     return empty_gdf(imgs_df_index_name, imgs_df_cols_and_index_types, crs_epsg_code=crs_epsg_code)
 
 
-def empty_polygons_df(polygons_df_index_name: str,
-                        polygons_df_cols_and_index_types: Dict[str, str],
-                        crs_epsg_code: int=STANDARD_CRS_EPSG_CODE) -> gpd.GeoDataFrame:
+def empty_polygons_df(
+        polygons_df_index_name : str,
+        polygons_df_cols_and_index_types : Dict[str, str],
+        crs_epsg_code : int=STANDARD_CRS_EPSG_CODE
+        ) -> GeoDataFrame:
     """Return a generic empty polygons_df GeoDataFrame conforming to the ImgPolygonAssociator format.
 
     Return a generic empty polygons_df GeoDataFrame conforming to the ImgPolygonAssociator format.
@@ -70,7 +75,7 @@ def empty_polygons_df(polygons_df_index_name: str,
     return empty_gdf(polygons_df_index_name, polygons_df_cols_and_index_types, crs_epsg_code=crs_epsg_code)
 
 
-def empty_gdf_same_format(source_df: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
+def empty_gdf_same_format_as(source_df: GeoDataFrame) -> GeoDataFrame:
     """
     Creates an empty geodataframe of the same format (index name, columns, column types) as the source_df argument.
 
@@ -92,7 +97,7 @@ def empty_gdf_same_format(source_df: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
     return new_empty_df
 
 
-def empty_polygons_df_same_format_as(polygons_df: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
+def empty_polygons_df_same_format_as(polygons_df: GeoDataFrame) -> GeoDataFrame:
     """
     Creates an empty polygons_df of the same format (index name, columns, column types) as the polygons_df argument.
 
@@ -101,10 +106,10 @@ def empty_polygons_df_same_format_as(polygons_df: gpd.GeoDataFrame) -> gpd.GeoDa
     :return: New empty dataframe
     """
 
-    return empty_gdf_same_format(polygons_df)
+    return empty_gdf_same_format_as(polygons_df)
 
 
-def empty_imgs_df_same_format_as(imgs_df: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
+def empty_imgs_df_same_format_as(imgs_df: GeoDataFrame) -> GeoDataFrame:
     """
     Creates an empty imgs_df of the same format (index name, columns, column types) as the imgs_df argument.
 
@@ -113,40 +118,7 @@ def empty_imgs_df_same_format_as(imgs_df: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
     :return: New empty images datagrame
     """
 
-    return empty_gdf_same_format(imgs_df)
-
-
-def empty_assoc_same_format_as(target_data_dir: Union[str, Path],
-                                source_data_dir: Optional[Union[str, Path]]=None,
-                                source_assoc: Optional[ImgPolygonAssociator]=None) -> ImgPolygonAssociator:
-    """
-    Creates an empty associator with data_dir target_data_dir of the same format as an existing one in source_data_dir or one given as source_assoc (same polygons_df and imgs_df columns and index names and paramaters).
-
-    :param target_data_dir: New associator data directory
-    :param source_data_dir: Source associator data directory
-    :param source_assoc: optional source associator
-
-    :returns: new associator
-    """
-
-    # exactly one of source_data_dir or source_assoc should be given
-    assert (source_data_dir != None) ^ (source_assoc != None)
-
-    if source_assoc == None:
-        source_assoc = ImgPolygonAssociator(source_data_dir)
-
-    # new empty polygons_df
-    new_empty_polygons_df = empty_polygons_df_same_format_as(source_assoc.polygons_df)                                        
-    
-    # new empty imgs_df
-    new_empty_imgs_df = empty_imgs_df_same_format_as(source_assoc.imgs_df) 
-
-    new_empty_assoc = ImgPolygonAssociator(data_dir=Path(target_data_dir), 
-                        imgs_df=new_empty_imgs_df, 
-                        polygons_df=new_empty_polygons_df, 
-                        **source_assoc._params_dict)
-
-    return new_empty_assoc
+    return empty_gdf_same_format_as(imgs_df)
     
 
 def empty_graph() -> BipartiteGraph:
