251877074fbef35403e583eef49eda74ac31e627
Rustam Antia <rustam.antia@dida.do>
2021-10-31 13:52:51 +0000

    inital commit cut mixins

---
+++ rs_tools/cut/cut_every_img_to_grid_mixin.py
@@ -0,0 +1,165 @@
+"""
+Functions to cut datasets of GeoTiffs (or update previously cut datasets) by cutting each image in the source dataset to a grid of images.
+    - cut_dataset_img_to_grid_of_imgs. Updates a dataset of GeoTiffs that was created with new_tif_dataset_img2grid_imgs.
+    - update_dataset_img_to_grid_of_imgs: customizable general function to create or update datasets of GeoTiffs from existing ones by iterating over polygons.
+"""
+
+from __future__ import annotations
+from rs_tools.global_constants import DATA_DIR_SUBDIRS
+from typing import TYPE_CHECKING, Union, List, Tuple, Optional
+from rs_tools.cut.type_aliases import ImgSize
+import logging
+from pathlib import Path
+
+if TYPE_CHECKING:
+    from rs_tools.img_polygon_associator import ImgPolygonAssociator
+from rs_tools.cut.single_img_cutter_grid import ImgToGridCutter
+from rs_tools.cut.img_filter_predicates import AlwaysTrue
+
+logger = logging.getLogger(__name__)
+
+class CreateDSCutEveryImgToGridMixIn(object):
+
+    def cut_every_img_to_grid(
+            self,
+            target_data_dir : Union[str, Path],
+            new_img_size : ImgSize = 512,
+            img_bands : Optional[List[int]]=None,
+            label_bands : Optional[List[int]]=None
+            ) -> ImgPolygonAssociator:
+        """
+        Create a new dataset of GeoTiffs (images, labels, and associator) where each image is cut into a grid of images.
+
+        Exactly one of source_data_dir or source_assoc should be set (i.e. not None).
+
+        Args:
+            target_data_dir (Union[str, Path]): path to data directory where the new dataset (images, labels, associator) will be created. If the directory does not exist it will be created.
+            new_img_size (ImgSize): size of new images (side length or (rows, col)) for 'centered' and 'random' modes. Defaults to 512.
+            img_bands (List[int], optional): list of bands to extract from source images. Defaults to None (i.e. all bands).
+            label_bands (List[int], optional):  list of bands to extract from source labels. Defaults to None (i.e. all bands).
+
+        Returns:
+            ImgPolygonAssociator: associator of new dataset in target_data_dir
+
+        Warning:
+            Currently only works if the source associator component files are in the standard locations determined by the source_data_dir arg.
+        """
+
+        return self._create_or_update_cut_every_img_to_grid(
+                    create_or_update='create',
+                    target_data_dir=target_data_dir,
+                    new_img_size=new_img_size,
+                    img_bands=img_bands,
+                    label_bands=label_bands
+        )
+
+
+    def _update_cut_every_img_to_grid(self) -> None:
+        """
+        Update a dataset of GeoTiffs (images, labels, and associator) where each image is cut into a grid of images.
+
+        Warning:
+            Make sure this does exactly what you want when updating an existing data_dir (e.g. if new polygons have been addded to the source_data_dir that overlap with existing labels in the target_data_dir these labels will not be updated. This should be fixed!). It might be safer to just recut the source_data_dir.
+        """
+
+        required_cut_params = {
+            'source_data_dir',
+            'new_img_size',
+            'img_bands',
+            'label_bands'
+        }
+
+        if not required_cut_params <= set(self._update_from_source_dataset_dict.keys()):
+            raise KeyError(f"The following cut params are missing: {set(self._update_from_source_dataset_dict.keys()) - required_cut_params}")
+
+        self._create_or_update_cut_every_img_to_grid(
+            create_or_update='update',
+            source_data_dir=self._update_from_source_dataset_dict['source_data_dir'],
+            new_img_size=self._update_from_source_dataset_dict['new_img_size'],
+            img_bands=self._update_from_source_dataset_dict['img_bands'],
+            label_bands=self._update_from_source_dataset_dict['label_bands']
+        )
+
+
+    def _create_or_update_cut_every_img_to_grid(
+            self,
+            create_or_update : str,
+            source_data_dir : Union[str, Path] = None,
+            target_data_dir : Union[str, Path] = None,
+            new_img_size : ImgSize = 512,
+            img_bands : Optional[List[int]]=None,
+            label_bands : Optional[List[int]]=None
+            ) -> Optional[ImgPolygonAssociator]:
+        """
+        Create a new dataset of GeoTiffs (images, labels, and associator) where each image is cut into a grid of images.
+
+        Args:
+            source_data_dir (Union[str, Path], optional): data directory (images, labels, associator) containing the GeoTiffs to be cut from.
+            source_assoc (ImgPolygonAssociator, optional): associator of dataset containing the GeoTiffs to be cut from.
+            target_data_dir (Union[str, Path]): path to data directory where the new dataset (images, labels, associator) will be created. If the directory does not exist it will be created.
+            target_assoc (ImgPolygonAssociator, optional): associator of target dataset.
+            new_img_size (ImgSize): size of new images (side length or (rows, col)) for 'centered' and 'random' modes. Defaults to 512.
+            img_bands (List[int], optional): list of bands to extract from source images. Defaults to None (i.e. all bands).
+            label_bands (List[int], optional):  list of bands to extract from source labels. Defaults to None (i.e. all bands).
+
+        Returns:
+            ImgPolygonAssociator: associator of new dataset in target_data_dir
+        """
+
+        # TODO factor out, code same for iter over imgs
+        if not create_or_update in {'create', 'update'}:
+            raise ValueError(f"Unknown create_or_update arg {create_or_update}, should be one of 'create', 'update'.")
+
+        if create_or_update == 'create':
+
+            # Check args
+            if source_data_dir is not None:
+                raise ValueError(f"TODOTODO")
+            if target_data_dir is None:
+                raise ValueError("TODOTODO")
+
+            source_assoc = self
+
+        elif create_or_update == 'update':
+
+            # Check args
+            if target_data_dir is not None:
+                raise ValueError(f"TODOTODO")
+            if source_data_dir is None:
+                raise ValueError("TODOTODO")
+
+            source_assoc = self.__class__.from_data_dir(source_data_dir)
+
+        target_data_dir = Path(target_data_dir)
+
+        img2grid_cutter = ImgToGridCutter(
+                                source_assoc=source_assoc,
+                                target_images_dir=target_data_dir / 'images',
+                                target_labels_dir=target_data_dir / 'labels',
+                                new_img_size=new_img_size,
+                                img_bands=img_bands,
+                                label_bands=label_bands)
+        always_true = AlwaysTrue()
+
+        target_assoc = self.create_or_update_dataset_iter_over_imgs(
+                            create_or_update=create_or_update,
+                            source_data_dir=source_data_dir,
+                            target_data_dir=target_data_dir,
+                            img_cutter=img2grid_cutter,
+                            img_filter_predicate=always_true)
+
+        # remember the cutting params.
+        target_assoc._update_from_source_dataset_dict.update(
+            {
+                'update_method' : 'update_dataset_every_img_to_grid',
+                'source_data_dir' : source_assoc.images_dir.parent, # Assuming standard data directory format
+                'new_img_size' :  new_img_size,
+                'img_bands' : img_bands,
+                'label_bands' : label_bands,
+            }
+        )
+        target_assoc._params_dict['img_size'] = new_img_size
+        target_assoc.save()
+
+        if create_or_update == 'create':
+            return target_assoc
---
+++ rs_tools/cut/cut_iter_over_imgs_mixin.py
@@ -0,0 +1,166 @@
+"""
+Customizable general function to create or update datasets of GeoTiffs from existing ones by iterating over images.
+"""
+
+from __future__ import annotations
+from typing import Union, List, Any, Optional, TYPE_CHECKING
+import logging
+from pathlib import Path
+from tqdm import tqdm
+import pandas as pd
+from geopandas import GeoDataFrame
+
+if TYPE_CHECKING:
+    from rs_tools import ImgPolygonAssociator
+from rs_tools.global_constants import DATA_DIR_SUBDIRS
+from rs_tools.cut.single_img_cutter_base import SingleImgCutter
+from rs_tools.cut.img_filter_predicates import ImgFilterPredicate
+from rs_tools.cut.img_filter_predicates import AlwaysTrue as AlwaysTrueImgs
+
+logger = logging.getLogger(__name__)
+
+class CreateDSCutIterOverImgsMixIn(object):
+
+    def create_or_update_dataset_iter_over_imgs(
+            self,
+            create_or_update : str,
+            img_cutter : SingleImgCutter,
+            source_data_dir : Optional[Union[str, Path]] = None,
+            target_data_dir : Optional[Union[str, Path]] = None,
+            img_filter_predicate : ImgFilterPredicate = AlwaysTrueImgs()
+            ) -> ImgPolygonAssociator:
+        """
+        Create or update a data set of GeoTiffs by iterating over images in the source dataset.
+
+        Create or update a data set of GeoTiffs (images, labels, and associator) in target_data_dir from the data set of GeoTiffs in source_data_dir by iterating over the images in the source dataset/associator that have not been cut to images in the target_data_dir (i.e. all images if the target dataset doesn not exist yet), filtering the images using the img_filter_predicate, and then cutting using an img_cutter.
+
+        Note:
+            Exactly one of the source_data_dir and source_assoc arguments needs to be set (i.e. not None) and simillarly for target_data_dir and target_assoc.
+
+        Warning:
+            Make sure this does exactly what you want when updating an existing data_dir (e.g. if new polygons have been addded to the source_data_dir that overlap with existing labels in the target_data_dir these labels will not be updated. This should be fixed!). It might be safer to just recut the source_data_dir.
+
+        Args:
+            create_or_update (str) : One of 'create' or 'update'.
+            source_data_dir (Union[str, Path]): data directory (images, labels,
+                associator) containing the GeoTiffs to be cut from.
+                Use only if updating a dataset.
+            target_data_dir (Union[str, Path]): data directory of target
+                dataset. Use only if creating a dataset.
+            img_cutter (SingleImgCutter): single image cutter used to cut
+                selected images in the source dataset.
+            img_filter_predicate (ImgFilterPredicate, optional):
+                predicate to filter images. Defaults to AlwaysTrue().
+        """
+
+        if not create_or_update in {'create', 'update'}:
+            raise ValueError(f"Unknown create_or_update arg {create_or_update}, should be one of 'create', 'update'.")
+
+        if create_or_update == 'create':
+
+            # Check args
+            if source_data_dir is not None:
+                raise ValueError(f"TODOTODO")
+            if target_data_dir is None:
+                raise ValueError("TODOTODO")
+
+            # Create source_assoc
+            source_assoc = self
+
+            # Create target assoc, ...
+            target_assoc = self.empty_assoc_same_format_as(target_data_dir)
+            target_assoc._update_from_source_dataset_dict['cut_imgs'] = []
+
+            # ..., image data dirs, ...
+            for dir in target_assoc.image_data_dirs:
+                dir.mkdir(parents=True, exist_ok=True)
+
+            # ... and the associator dir.
+            target_assoc.assoc_dir.mkdir(parents=True, exist_ok=True)
+
+            # Make sure no associator files already exist.
+            if list(target_assoc.assoc_dir.iterdir()) != []:
+                raise Exception(f"The assoc_dir in {target_assoc.assoc_dir} should be empty!")
+
+        elif create_or_update == 'update':
+
+            # Check args
+            if target_data_dir is not None:
+                raise ValueError(f"TODOTODO")
+            if source_data_dir is None:
+                raise ValueError("TODOTODO")
+
+            target_assoc = self
+            source_assoc = self.__class__.from_data_dir(source_data_dir)
+
+        # Remember information to determine for which images to generate new labels
+        imgs_in_target_dataset_before_update = set(target_assoc.imgs_df.index)
+        added_polygons = []  # updated as we iterate
+
+        # dict to temporarily store information which will be appended to target_assoc's imgs_df after cutting
+        new_imgs_dict = {index_or_col_name: [] for index_or_col_name in [source_assoc.imgs_df.index.name] + list(source_assoc.imgs_df.columns)}
+
+        target_assoc.add_to_polygons_df(source_assoc.polygons_df)
+
+        imgs_in_src_that_have_been_cut = target_assoc._update_from_source_dataset_dict['cut_imgs']
+        mask_imgs_in_src_that_have_not_been_cut = ~source_assoc.imgs_df.index.isin(imgs_in_src_that_have_been_cut)
+        names_of_imgs_in_src_that_have_not_been_cut = source_assoc.imgs_df.loc[mask_imgs_in_src_that_have_not_been_cut].index
+
+        # Iterate over all images in source dataset that have not been cut.
+        for img_name in tqdm(names_of_imgs_in_src_that_have_not_been_cut, desc='Cutting dataset: '):
+
+            # If filter condition is satisfied, (if not, don't do anything) ...
+            if img_filter_predicate(
+                    img_name,
+                    target_assoc=target_assoc,
+                    new_img_dict=new_imgs_dict,
+                    source_assoc=source_assoc):
+
+                # ... cut the images (and their labels) and remember information to be appended to target_assoc imgs_df in return dict
+                imgs_from_single_cut_dict = img_cutter(
+                                                img_name=img_name,
+                                                new_polygons_df=target_assoc.polygons_df,
+                                                new_graph=target_assoc._graph)
+
+                # Make sure img_cutter returned dict with same keys as needed by new_imgs_dict.
+                assert set(imgs_from_single_cut_dict.keys()) == set(target_assoc.imgs_df.columns) | {target_assoc.imgs_df.index.name}, f"dict returned by img_cutter doesn't contain the same keys as needed by new_imgs_dict!"
+
+                # Accumulate information for the new imgs in new_imgs_dict.
+                for key in new_imgs_dict.keys():
+                            new_imgs_dict[key] += (imgs_from_single_cut_dict[key])
+
+                new_img_names = imgs_from_single_cut_dict[source_assoc.imgs_df.index.name]
+                img_bounding_rectangles = imgs_from_single_cut_dict['geometry']
+                for new_img_name, img_bounding_rectangle in zip(new_img_names, img_bounding_rectangles):
+
+                    # Update target_assoc._update_from_source_dataset_dict
+                    target_assoc._update_from_source_dataset_dict['cut_imgs'].append(img_name)
+
+                    # Update graph and modify polygons_df in target_assoc
+                    target_assoc._add_img_to_graph_modify_polygons_df(
+                        img_name=new_img_name,
+                        img_bounding_rectangle=img_bounding_rectangle)
+
+        # Extract accumulated information about the imgs we've created in the target dataset into a dataframe...
+        new_imgs_df = GeoDataFrame(
+                        new_imgs_dict,
+                        crs=target_assoc.imgs_df.crs)
+        new_imgs_df.set_index(target_assoc.imgs_df.index.name, inplace=True)
+
+        # ... and append it to self.imgs_df.
+        data_frames_list = [target_assoc.imgs_df, new_imgs_df]
+        target_assoc.imgs_df = GeoDataFrame(pd.concat(data_frames_list), crs=data_frames_list[0].crs)
+
+        # For those images that existed before the update and now intersect with newly added polygons ...
+        imgs_w_new_polygons = [img_name for polygon_name in added_polygons for img_name in target_assoc.imgs_intersecting_polygon(polygon_name) if img_name in imgs_in_target_dataset_before_update]
+        # Delete the old labels (since they won't show the new polygons)...
+        for img_name in imgs_w_new_polygons:
+            label_path = target_assoc.labels_dir / img_name
+            label_path.unlink(missing_ok=True)
+        # ... and generate new ones.
+        target_assoc.make_labels(img_names=imgs_w_new_polygons)
+
+        # Finally, save associator to disk.
+        target_assoc.save()
+
+        return target_assoc
