208e9159ed6f9ce29a01a820d5df9c7db5ed8b57
Rustam Antia <rustam.antia@dida.do>
2022-01-10 11:19:03 +0100

    single_img_cutter_bbox: get img_bounds from directly from img instead of from imgs_df to avoid crs conversion (more precise)

--- rs_tools/cut/single_img_cutter_bbox.py
+++ rs_tools/cut/single_img_cutter_bbox.py
@@ -1,4 +1,4 @@
-""" 
+"""
 SingleImgCutter that cuts a small image from a predefined bounding box on the image accepted by the polygon filter predicate.
 """
 from __future__ import annotations
@@ -7,7 +7,7 @@ from rs_tools.cut.type_aliases import ImgSize
 import logging
 from pathlib import Path
 from shapely.geometry import Polygon, box
-from geopandas import GeoDataFrame 
+from geopandas import GeoDataFrame
 import rasterio as rio
 from rasterio.windows import from_bounds, Window
 
@@ -28,13 +28,13 @@ class ToImgBBoxCutter(SingleImgCutter):
     SingleImgCutter that cuts an image using a predefined GeoDataFrame of bounding boxes.
     """
 
-    def __init__(self, 
-            source_assoc : ImgPolygonAssociator, 
-            target_images_dir : Union[Path, str], 
-            target_labels_dir : Union[Path, str], 
+    def __init__(self,
+            source_assoc : ImgPolygonAssociator,
+            target_images_dir : Union[Path, str],
+            target_labels_dir : Union[Path, str],
             new_img_size : Optional[ImgSize],
-            bounding_boxes : GeoDataFrame, 
-            img_bands : Optional[List[int]] = None, 
+            bounding_boxes : GeoDataFrame,
+            img_bands : Optional[List[int]] = None,
             label_bands : Optional[List[int]] = None
             ) -> None:
         """
@@ -56,16 +56,16 @@ class ToImgBBoxCutter(SingleImgCutter):
         :param label_bands: list of bands to extract from source labels. Defaults to None (i.e. all bands).
         :type label_bands: Optional[List[int]]
         """
-        
-        super().__init__(source_assoc=source_assoc, 
-                        target_images_dir=target_images_dir, 
-                        target_labels_dir=target_labels_dir, 
-                        img_bands=img_bands, 
-                        label_bands=label_bands)                        
-        
+
+        super().__init__(source_assoc=source_assoc,
+                        target_images_dir=target_images_dir,
+                        target_labels_dir=target_labels_dir,
+                        img_bands=img_bands,
+                        label_bands=label_bands)
+
         # Save bounding boxes
         self.bounding_boxes = bounding_boxes
-        
+
         # Check new_img_size arg type
         if not isinstance(new_img_size, int) or (isinstance(new_img_size, tuple) and len(new_img_size)==2 and all(isinstance(entry, int) for entry in new_img_size)): 
             raise TypeError("new_img_size needs to be an integer or a pair of integers!")
@@ -80,13 +80,13 @@ class ToImgBBoxCutter(SingleImgCutter):
         if not self.new_img_size_rows > 0:
             logger.error("new_img_size needs to have positive side length(s)")
             raise ValueError("new_img_size needs to have positive side length(s)")
-        if not self.new_img_size_cols > 0: 
+        if not self.new_img_size_cols > 0:
             logger.error("new_img_size needs to have positive side length(s)")
             raise ValueError("new_img_size needs to have positive side length(s)")
 
-    def _get_windows_transforms_img_names(self, 
-            source_img_name: str, 
-            target_assoc : Optional[ImgPolygonAssociator] = None, 
+    def _get_windows_transforms_img_names(self,
+            source_img_name: str,
+            target_assoc : Optional[ImgPolygonAssociator] = None,
             new_imgs_dict : Optional[dict] = None,
             **kwargs : Any
             ) -> List[str]:
@@ -94,28 +94,27 @@ class ToImgBBoxCutter(SingleImgCutter):
         source_img_path = self.source_assoc._images_dir / source_img_name
 
         with rio.open(source_img_path) as src:
-            img_bounds = self.source_assoc.imgs_df.to_crs(src.crs).geometry.loc[source_img_name]
+            img_bounds = box(*src.bounds)
             bounding_boxes = self.bounding_boxes.to_crs(src.crs)
             bounding_boxes = bounding_boxes.loc[bounding_boxes.geometry.within(img_bounds)]
-            
             windows_transforms_img_names = []
             for i, geometry in enumerate(bounding_boxes.geometry):
                 initial_window = from_bounds(*geometry.bounds, src.transform)
-                
+
                 new_col_off = _correct_window_offset(initial_window.col_off,
                                                      initial_window.width,
                                                      self.new_img_size_cols)
-                
+
                 new_row_off = _correct_window_offset(initial_window.row_off,
                                                      initial_window.height,
                                                      self.new_img_size_rows)
-                
+
                 window = Window(new_col_off, new_row_off,
                                 self.new_img_size_cols, self.new_img_size_rows)
-                
+
                 window_transform = src.window_transform(window)
                 new_img_name = f"{Path(source_img_name).stem}_{i}.tif"
-                
+
                 windows_transforms_img_names.append((window, window_transform, new_img_name))
 
         return windows_transforms_img_names
