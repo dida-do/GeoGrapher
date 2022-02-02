617493413edeec4bf2f9b4adb7bdf2fba4fb2bcc
Rustam Antia <rustam.antia@gmail.com>
2022-02-02 13:45:39 +0100

    formating

--- rs_tools/labels/label_type_conversion_utils.py
+++ rs_tools/labels/label_type_conversion_utils.py
@@ -9,7 +9,7 @@ def convert_polygons_df_soft_cat_to_cat(polygons_df: GeoDataFrame) -> GeoDataFra
 
     # make 'type' column
     new_polygons_df['type'] = new_polygons_df[[col for col in new_polygons_df.columns if col[:15] == 'prob_seg_class_']].idxmax(axis='columns').apply(lambda x: x[15:])
-    
+
     # drop 'prob_seg_class_[seg_class]' cols
     new_polygons_df.drop([col for col in new_polygons_df.columns if col[:15] == 'prob_seg_class_'], axis='columns', inplace=True)
 
