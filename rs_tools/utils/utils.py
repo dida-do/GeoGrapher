608931517e3563e8da8c4a751c28f6acd77279bc
Rustam Antia <rustam.antia@dida.do>
2022-02-02 13:45:09 +0100

    util: create logger

--- rs_tools/utils/utils.py
+++ rs_tools/utils/utils.py
@@ -12,6 +12,7 @@ import copy
 import numpy as np
 import rasterio.mask
 import rasterio as rio
+import logging
 import os
 import geopandas as gpd
 from geopandas import GeoDataFrame
@@ -22,6 +23,46 @@ from shapely.ops import transform
 import pyproj
 
 GEOMS_UNION = Union[Point, Polygon, MultiPoint, MultiPolygon, MultiLineString, LinearRing, LineString, GeometryCollection]
+
+
+def create_logger(app_name: str, level: int = logging.INFO) -> logging.Logger:
+    """
+    Serves as a unified way to instantiate a new logger. Will create a new logging instance with the name app_name. The logging output is sent to the console via a logging.StreamHandler() instance. The output will be formatted using the logging time, the logger name, the level at which the logger was called and the logging message. As the root logger threshold is set to WARNING, the instantiation via logging.getLogger(__name__) results in a logger instance, which console handel also has the threshold set to WARNING. One needs to additionally set the console handler level to the desired level, which is done by this function.
+
+    ..note:: Function might be adapted for more specialized usage in the future
+
+    Args:
+        app_name (string): Name of the logger. Will appear in the console output
+        level (int): threshold level for the new logger.
+
+    Returns:
+        logging.Logger: new logging instance
+
+    Examples::
+
+    >>> import logging
+    >>> logger=create_logger(__name__,logging.DEBUG)  
+    """
+
+    # create new up logger
+    logger = logging.getLogger(app_name)
+    logger.setLevel(level)
+
+    # create console handler and set level
+    ch = logging.StreamHandler()
+    ch.setLevel(level)
+
+    # create formatter
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    # add formatter to the console handler
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+    return logger
+
+
 
 
 def transform_shapely_geometry(geometry: GEOMS_UNION,
