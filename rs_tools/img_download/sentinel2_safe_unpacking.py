c1e1b8eda7fb296687f4603e3fd7f0a729686b38
Rustam Antia <rustam.antia@dida.do>
2022-02-15 16:08:39 +0100

    safe unpacking: add zero bands for missing gml masks (new L2A format)

--- rs_tools/img_download/sentinel2_safe_unpacking.py
+++ rs_tools/img_download/sentinel2_safe_unpacking.py
@@ -8,33 +8,39 @@ from collections import OrderedDict
 from pathlib import Path
 from typing import Dict, List, Tuple, Union
 
-from scipy.ndimage import zoom
 import geopandas as gpd
 import numpy as np
 import rasterio as rio
 import rasterio.mask
-from rs_tools.utils.utils import create_logger
 from rasterio.enums import Resampling
+
+# from fiona.errors import DriverError
+from rasterio.errors import RasterioIOError
+from rs_tools.utils.utils import create_logger
+from scipy.ndimage import zoom
 from shapely.geometry import box
 
 NO_DATA_VAL = 0  # No data value for sentinel 2 L1C
 
-logger = create_logger(__name__)
+log = create_logger(__name__)
 
 
 def safe_to_geotif_L2A(
-        safe_root: Path,
-        resolution: Union[str, int],
-        upsample_lower_resolution: bool = True,
-        outdir: Path = None,
-        TCI: bool = True,
-        requested_jp2_masks: List[str] = ["CLDPRB", "SNWPRB"],
-        requested_gml_mask: List[Tuple[str]] = [("CLOUDS", "B00")],
-        # upsampling_method_specifier: str = "bilinear"
+    safe_root: Path,
+    resolution: Union[str, int],
+    upsample_lower_resolution: bool = True,
+    outdir: Path = None,
+    TCI: bool = True,
+    requested_jp2_masks: List[str] = ["CLDPRB", "SNWPRB"],
+    requested_gml_mask: List[Tuple[str]] = [("CLOUDS", "B00")],
+    # upsampling_method_specifier: str = "bilinear"
 ) -> Tuple[Dict, Dict]:
     """
-    Converts a .SAFE file with L2A sentinel-2 data to geotif and returns a dict 
+    Converts a .SAFE file with L2A sentinel-2 data to geotif and returns a dict
     with the crs epsg code and a shapely polygon defined by the image bounds.
+
+    Warning:
+        The L2A band structure changed in October 2021, new products do not contain gml masks anymore. In this
 
     ..note::
 
@@ -63,8 +69,7 @@ def safe_to_geotif_L2A(
     assert resolution in [10, 20, 60, "10", "20", "60"]
 
     # define output file
-    out_file_parent_dir = outdir if (outdir
-                                     and outdir.is_dir()) else safe_root.parent
+    out_file_parent_dir = outdir if (outdir and outdir.is_dir()) else safe_root.parent
     outfile = out_file_parent_dir / (safe_root.stem + "_TEMP.tif")
 
     granule_dir = safe_root / "GRANULE"
@@ -74,43 +79,36 @@ def safe_to_geotif_L2A(
     jp2_resolution = 20 if resolution in [10, "10"] else resolution
     jp2_mask_paths = list(
         filter(
-            lambda file: any(mask_name in file.name
-                             for mask_name in requested_jp2_masks) and
-                         f"{jp2_resolution}m" in file.name, masks_dir.glob("*.jp2")))
+            lambda file: any(mask_name in file.name for mask_name in requested_jp2_masks)
+            and f"{jp2_resolution}m" in file.name,
+            masks_dir.glob("*.jp2"),
+        )
+    )
 
     # Paths for S2 Bands
-    jp2_path_desired_resolution = granule_dir / "{}/IMG_DATA/R{}m/".format(
-        os.listdir(granule_dir)[0], resolution)
+    jp2_path_desired_resolution = granule_dir / "{}/IMG_DATA/R{}m/".format(os.listdir(granule_dir)[0], resolution)
 
-    tci_path = next(
-        filter(lambda path: path.stem.split("_")[-2] == "TCI",
-               jp2_path_desired_resolution.glob("*.jp2")))
+    tci_path = next(filter(lambda path: path.stem.split("_")[-2] == "TCI", jp2_path_desired_resolution.glob("*.jp2")))
 
     # add missing higher res jps paths
     img_data_bands = list(
-        filter(lambda path: path.stem.split("_")[-2] not in ["TCI"],
-               jp2_path_desired_resolution.glob("*.jp2")))
+        filter(lambda path: path.stem.split("_")[-2] not in ["TCI"], jp2_path_desired_resolution.glob("*.jp2"))
+    )
     out_default_reader = rio.open(img_data_bands[0], driver="JP2OpenJPEG")
 
     # include lower resolution bands
     if upsample_lower_resolution:
 
-        for higher_res in filter(lambda res: res > int(resolution),
-                                 [10, 20, 60]):
-            jp2_higher_res_path = granule_dir / "{}/IMG_DATA/R{}m/".format(
-                os.listdir(granule_dir)[0], higher_res)
+        for higher_res in filter(lambda res: res > int(resolution), [10, 20, 60]):
+            jp2_higher_res_path = granule_dir / "{}/IMG_DATA/R{}m/".format(os.listdir(granule_dir)[0], higher_res)
 
             img_data_bands += list(
                 filter(
-                    lambda path: path.stem.split("_")[-2] not in itertools.
-                        chain(
-                        map(
-                            lambda path: path.stem.split("_")[-2],
-                            img_data_bands
-                        ),
-                        ["TCI"]
-                    ),
-                    jp2_higher_res_path.glob("*.jp2")))
+                    lambda path: path.stem.split("_")[-2]
+                    not in itertools.chain(map(lambda path: path.stem.split("_")[-2], img_data_bands), ["TCI"]),
+                    jp2_higher_res_path.glob("*.jp2"),
+                )
+            )
 
     # # if we have both B08 and B8A remove B8A
     # if {'B8A', 'B08'} <= {name.name.split("_")[-2] for name in img_data_bands}:
@@ -124,21 +122,21 @@ def safe_to_geotif_L2A(
         band = rio.open(file, driver="JP2OpenJPEG")
         max_width = max(max_width, band.width)
         max_height = max(max_height, band.height)
-        bands_dict[file.stem.split("_")[-2]] = (
-            band, int(file.stem.split("_")[-1][:2]))
+        bands_dict[file.stem.split("_")[-2]] = (band, int(file.stem.split("_")[-1][:2]))
 
     # sort bands_dict
     bands_dict = OrderedDict(sorted(bands_dict.items()))
 
     # paths for gml masks
     gml_mask_paths = list(
-        filter(
-            lambda path: tuple(path.stem.split("_")[-2:]) in
-                         requested_gml_mask, masks_dir.glob("*.gml")))
-    gml_mask_paths_dict = {
-        tuple(path.stem.split("_")[-2:]): path
-        for path in gml_mask_paths
-    }
+        filter(lambda path: tuple(path.stem.split("_")[-2:]) in requested_gml_mask, masks_dir.glob("*.gml"))
+    )
+    gml_mask_paths_dict = {tuple(path.stem.split("_")[-2:]): path for path in gml_mask_paths}
+    # add invalid paths for missing gml masks (will result in zero bands later)
+    for pair in requested_gml_mask:
+        if pair not in gml_mask_paths_dict:
+            gml_mask_paths_dict[pair] = Path(f"/path/to/nowhere")
+        gml_mask_paths.append(Path(f"/path/to/nowhere"))
 
     # reader for tci
     tci_band = rio.open(tci_path, driver="JP2OpenJPEG")
@@ -148,30 +146,31 @@ def safe_to_geotif_L2A(
 
     # write geotif
     tif_band_names = {}
-    with rio.open(outfile,
-                  "w",
-                  driver="GTiff",
-                  width=max_width,
-                  height=max_width,
-                  count=count,
-                  crs=out_default_reader.crs,
-                  transform=out_default_reader.transform,
-                  dtype=out_default_reader.dtypes[0]) as dst:
+    with rio.open(
+        outfile,
+        "w",
+        driver="GTiff",
+        width=max_width,
+        height=max_width,
+        count=count,
+        crs=out_default_reader.crs,
+        transform=out_default_reader.transform,
+        dtype=out_default_reader.dtypes[0],
+    ) as dst:
 
         dst.nodata = NO_DATA_VAL
 
         # write gml masks
-        for idx, (gml_name,
-                  gml_path) in enumerate(gml_mask_paths_dict.items()):
+        for idx, (gml_name, gml_path) in enumerate(gml_mask_paths_dict.items()):
 
             try:
+                assert gml_path.is_file()
                 shapes = gpd.read_file(gml_path)["geometry"].values
-                mask, _, _ = rasterio.mask.raster_geometry_mask(
-                    out_default_reader, shapes, crop=False, invert=True)
-            except ValueError:
-                mask = np.full(shape=out_default_reader.read(1).shape,
-                               fill_value=0.0,
-                               dtype=np.uint16)
+                mask, _, _ = rasterio.mask.raster_geometry_mask(out_default_reader, shapes, crop=False, invert=True)
+            # in case mask is empty or does not exist:
+            except (ValueError, AssertionError, RasterioIOError):
+                log.info(f"using all zero band instead instead of gml mask {gml_name} for {safe_root.name}")
+                mask = np.full(shape=out_default_reader.read(1).shape, fill_value=0.0, dtype=np.uint16)
 
             band_idx = len(bands_dict) + 3 * TCI + idx + 1
             tif_band_names[band_idx] = "_".join(gml_name)
@@ -179,8 +178,7 @@ def safe_to_geotif_L2A(
             dst.write(mask.astype(np.uint16), band_idx)
 
         # write jp2 bands
-        for idx, (band_name, (dst_reader,
-                              res)) in enumerate(bands_dict.items()):
+        for idx, (band_name, (dst_reader, res)) in enumerate(bands_dict.items()):
 
             if res != int(resolution):
 
@@ -208,8 +206,7 @@ def safe_to_geotif_L2A(
             for i in range(3):
 
                 band_idx = i + 1
-                img = (tci_band.read(band_idx) * (65535.0 / 255.0)).astype(
-                    np.uint16)
+                img = (tci_band.read(band_idx) * (65535.0 / 255.0)).astype(np.uint16)
                 tif_band_names[band_idx] = f"tci_{band_idx}"
 
                 dst.write(img, band_idx)
@@ -224,7 +221,4 @@ def safe_to_geotif_L2A(
 
     outfile.rename(out_file_parent_dir / (safe_root.stem + ".tif"))
 
-    return {
-        'crs_epsg_code': crs_epsg_code,
-        'img_bounding_rectangle': img_bounding_rectangle
-    }
+    return {"crs_epsg_code": crs_epsg_code, "img_bounding_rectangle": img_bounding_rectangle}
