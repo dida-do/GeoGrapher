b364bd198c993cb8d81305517d0e0791b4512c50
Rustam Antia <rustam.antia@dida.do>
2022-03-23 19:25:46 +0100

    refactor sentinel2_mixin

--- rs_tools/img_download/sentinel2_mixin.py
+++ rs_tools/img_download/sentinel2_mixin.py
@@ -105,88 +105,21 @@ class Sentinel2DownloaderMixIn:
             info_dicts: A dictionary containing information about the images and polygons. ({'list_img_info_dicts': [img_info_dict], 'polygon_info_dict': polygon_info_dict})
 
         Raises:
-            LookupError: Raised if an unkknown product type is given.
+            ValueError: Raised if an unkknown product type is given.
             NoImgsForPolygonFoundError: Raised if no downloadable images with cloud coverage less than or equal to max_percent_cloud_coverage could be found for the polygon.
-            KeyError: Raised if the product name could not be extracted correctly.
-            ImgAlreadyExistsError: Raised if the image selected to download already exists in the associator.
-            ImgDownloadError: Raised if an error occurred while trying to download a product.
         """
 
-        for s2_specific_keyword_arg, value in {
-            ('producttype', producttype), ('resolution', resolution),
-            ('max_percent_cloud_coverage', max_percent_cloud_coverage),
-            ('area_relation', area_relation), ('date', date)
-        }:
-
-            if value is None:
-                try:
-                    # Use saved value
-                    value = getattr(self,
-                                    f"sentinel2_{s2_specific_keyword_arg}")
-                except (AttributeError, KeyError):
-                    raise ValueError(
-                        f"Need to set {s2_specific_keyword_arg} keyword argument for the sentinel2 downloader."
-                    )
-            else:
-                # Remember value
-                setattr(self, f"sentinel2_{s2_specific_keyword_arg}", value)
-
-        # Run some safety checks on the arg values
-        if resolution not in {10, 20, 60}:
-            raise ValueError(f"Unknown resolution: {resolution}")
-        if max_percent_cloud_coverage < 0 or max_percent_cloud_coverage > 100:
-            raise ValueError(
-                f"Unknown max_percent_cloud_coverage: {max_percent_cloud_coverage}"
-            )
-        if producttype not in {'L1C', 'S2MSI1C', 'L2A', 'S2MSI2A'}:
-            raise ValueError(f"Unknown producttype: {producttype}")
-
-        # producttype abbreviations
-        if producttype == 'L1C':
-            producttype = 'S2MSI1C'
-        if producttype == 'L2A':
-            producttype = 'S2MSI2A'
-
-        # Get username and password to set up the sentinel API ...
-        try:
-            if None in {username, password}:
-                config = configparser.ConfigParser()
-                config_path = self.assoc_dir / "sentinel_scihub.ini"
-                if not config_path.is_file():
-                    raise FileNotFoundError
-                config.read(self.assoc_dir / "sentinel_scihub.ini")
-            if username is None:
-                username = config["login"]["username"]
-            if password is None:
-                password = config["login"]["password"]
-        except FileNotFoundError:
-            log.error(
-                f"Missing 'sentinel_scihub.ini' file in {self.assoc_dir}. Need API credentials (username and password)"
-            )
-        except KeyError as e:
-            log.error(
-                f"{e}: Missing entry in 'sentinel_scihub.ini' file. Need API credentials."
-            )
-
-        # ... and instantiate the API.
-        api = SentinelAPI(username, password)
-
-        # Return dicts with values to be collected in calling associator.
-        img_info_dict = {}
+        # (Save args / replace by saved value if None)
+        producttype, resolution, max_percent_cloud_coverage, date, area_relation = \
+            self._get_and_remember_args(producttype, resolution, max_percent_cloud_coverage, date, area_relation)
+        self._check_args_are_valid(producttype, resolution,
+                                   max_percent_cloud_coverage)
 
         # Determine missing args for the sentinel query.
-        rectangle_wkt = wkt.dumps(polygon_geometry.envelope)
-        # area_relation='Contains'
-        # date = ("NOW-364DAYS", "NOW") # anything older is in long-term archive
-        # Allow for producttype shorthand.
-        if producttype in {'L2A', 'S2MSI2A'}:
-            producttype = 'S2MSI2A'
-        elif producttype in {'L1C', 'S2MSI1C'}:
-            producttype = 'S2MSI1C'
-        else:
-            raise LookupError(
-                f"ImgPolygonAssociator.__init__: unknown producttype: {producttype}"
-            )
+        rectangle_wkt: str = wkt.dumps(polygon_geometry.envelope)
+        producttype = self._get_longform_producttype(producttype)
+
+        api = self._get_api(username, password)
 
         try:
 
@@ -200,37 +133,41 @@ class Sentinel2DownloaderMixIn:
 
             products = {k: v for k, v in products.items() if api.is_online(k)}
 
-        except (UnauthorizedError, ServerError, QueryLengthError, QuerySyntaxError) as e:
+        except (UnauthorizedError, ServerError) as e:
             log.exception(str(e))
             raise
-        # The sentinelsat API can throw an exception if there are no results for a query instead of returning an empty dict ...
-        except Exception as e:
-            # ... so in that case we set the result by hand:
-            products = {}
+        # # The sentinelsat API can throw an exception if there are no results for a query instead of returning an empty dict ...
+        # except Exception as e:
+        #     # ... so in that case we set the result by hand:
+        #     log.exception(str(e))
+        #     products = {}
 
         # If we couldn't find anything, remember that, so we can deal with it later.
         if len(products) == 0:
-
             raise NoImgsForPolygonFoundError(
                 f"No images for polygon {polygon_name} found with cloud coverage less than or equal to {max_percent_cloud_coverage}!"
             )
 
-        # If the query was succesful ...
+        # Return dicts with values to be collected in calling associator.
+        img_info_dict = {}
+
+        # If the query was succesful, ...
         products_list = list(products.keys())
         products_list = sorted(
             products_list, key=lambda x: products[x]["cloudcoverpercentage"])
+        # ... iterate over the products ordered by cloud coverage
         for product_id in products_list:
-            # ... and determine the file name.
+
             product_metadata = api.get_product_odata(product_id, full=True)
-            # (this key might have to be 'filename' (minus the .SAFE at the end) for L1C products?)
+
             try:
+                # (this key might have to be 'filename' (minus the .SAFE at the end) for L1C products?)
                 img_name = product_metadata['title'] + ".tif"
             except:
                 raise Exception(
                     f"Couldn't get the filename. Are you trying to download L1C products? Try changing the key for the products dict in the line of code above this..."
                 )
 
-            # If the file has been downloaded before (really, this should not happen, since EXPLAIN!), throw an error ...
             if img_name not in previously_downloaded_imgs_set:
                 try:
                     api.download(product_id, directory_path=download_dir)
@@ -246,13 +183,88 @@ class Sentinel2DownloaderMixIn:
                         'Date'].strftime("%Y-%m-%d-%H:%M:%S")
 
                     return {'list_img_info_dicts': [img_info_dict]}
-
-                except:
-                    log.warn(f"failed to download {product_metadata['title']}")
+                except Exception as e:
+                    log.warn(
+                        f"Failed to download or unzip {product_metadata['title']}: {str(e)}"
+                    )
 
         raise NoImgsForPolygonFoundError(
-            f"Either no images were found for {polygon_name} found or all images failed to download."
-        )
+            f"All images for {polygon_name} failed to download.")
+
+    def _get_longform_producttype(self, producttype):
+        """Return producttype in longform as needed by the sentinel API"""
+        if producttype in {'L2A', 'S2MSI2A'}:
+            producttype = 'S2MSI2A'
+        elif producttype in {'L1C', 'S2MSI1C'}:
+            producttype = 'S2MSI1C'
+        else:
+            raise ValueError(f"Unknown producttype: {producttype}")
+
+        return producttype
+
+    def _check_args_are_valid(self, producttype, resolution,
+                              max_percent_cloud_coverage):
+        """Run some safety checks on the arg values"""
+        if resolution not in {10, 20, 60}:
+            raise ValueError(f"Unknown resolution: {resolution}")
+        if max_percent_cloud_coverage < 0 or max_percent_cloud_coverage > 100:
+            raise ValueError(
+                f"Unknown max_percent_cloud_coverage: {max_percent_cloud_coverage}"
+            )
+        if producttype not in {'L1C', 'S2MSI1C', 'L2A', 'S2MSI2A'}:
+            raise ValueError(f"Unknown producttype: {producttype}")
+
+    def _get_api(self, username, password):
+        # Get username and password to set up the sentinel API ...
+        try:
+            if None in {username, password}:
+                config = configparser.ConfigParser()
+                config_path = self.assoc_dir / "sentinel_scihub.ini"
+                if not config_path.is_file():
+                    raise FileNotFoundError
+                config.read(self.assoc_dir / "sentinel_scihub.ini")
+            if username is None:
+                username = config["login"]["username"]
+            if password is None:
+                password = config["login"]["password"]
+        except FileNotFoundError:
+            # TODO: raise ValueError ?
+            log.error(
+                f"Missing 'sentinel_scihub.ini' file in {self.assoc_dir}. Need API credentials (username and password)"
+            )
+        except KeyError as e:
+            log.error(
+                f"{e}: Missing entry in 'sentinel_scihub.ini' file. Need API credentials."
+            )
+
+        # ... and instantiate the API.
+        api = SentinelAPI(username, password)
+
+        return api
+
+    def _get_and_remember_args(self, producttype, resolution,
+                               max_percent_cloud_coverage, date,
+                               area_relation):
+        """Replace saved arg values for those values that are None and save arg values that are not None"""
+        for s2_specific_keyword_arg, value in {
+            ('producttype', producttype), ('resolution', resolution),
+            ('max_percent_cloud_coverage', max_percent_cloud_coverage),
+            ('area_relation', area_relation), ('date', date)
+        }:
+            if value is None:
+                try:
+                    # Use saved value
+                    value = getattr(self,
+                                    f"sentinel2_{s2_specific_keyword_arg}")
+                except (AttributeError, KeyError):
+                    raise ValueError(
+                        f"Need to set {s2_specific_keyword_arg} keyword argument for the sentinel2 downloader."
+                    )
+            else:
+                # Remember value
+                setattr(self, f"sentinel2_{s2_specific_keyword_arg}", value)
+
+        return producttype, resolution, max_percent_cloud_coverage, date, area_relation
 
     def _process_downloaded_img_file_sentinel2(self, img_name: str,
                                                in_dir: Union[str, Path],
@@ -276,39 +288,31 @@ class Sentinel2DownloaderMixIn:
             return_dict: Contains information about the downloaded product.
         """
 
-        # file names and paths
         filename_no_extension = Path(img_name).stem
         zip_filename = filename_no_extension + ".zip"
-        safe_path = os.path.abspath(in_dir / Path("safe_files") /
-                                    (filename_no_extension + ".SAFE"))
-        zip_path = os.path.abspath(in_dir / zip_filename)
+        safe_path = Path(in_dir) / f"safe_files/{filename_no_extension}.SAFE"
+        zip_path = Path(in_dir) / zip_filename
 
-        # extract zip to .SAFE and delete zip
+        # extract zip to SAFE
         with ZipFile(zip_path) as zip_ref:
             zip_ref.extractall(in_dir / Path("safe_files/"))
         os.remove(zip_path)
-
         # convert SAFE to GeoTiff
         conversion_dict = safe_to_geotif_L2A(safe_root=Path(safe_path),
                                              resolution=resolution,
                                              outdir=out_dir)
 
-        orig_crs_epsg_code, img_bounding_rectangle_orig_crs = conversion_dict[
-            "crs_epsg_code"], conversion_dict["img_bounding_rectangle"]
-
-        # convert img_bounding_rectangle to the standard crs
-        img_bounding_rectangle = transform_shapely_geometry(
+        orig_crs_epsg_code = int(conversion_dict["crs_epsg_code"])
+        img_bounding_rectangle_orig_crs = conversion_dict[
+            "img_bounding_rectangle"]
+        img_bounding_rectangle = transform_shapely_geometry(  # convert to standard crs
             img_bounding_rectangle_orig_crs,
             from_epsg=orig_crs_epsg_code,
             to_epsg=convert_to_crs_epsg)
-        # try using rio.warp.transform_bounds?? maybe this is more accurate when comparing to APIfootprint??
-
-        # return a dict
-        return_dict = {
+        # try using rio.warp.transform_bounds instead?? maybe more accurate when comparing to APIfootprint??
+        return {
             'img_name': img_name,
             'geometry': img_bounding_rectangle,
-            'orig_crs_epsg_code': int(orig_crs_epsg_code),
-            'img_processed?': True
+            'orig_crs_epsg_code': orig_crs_epsg_code,
+            'img_processed?': True,
         }
-
-        return return_dict
