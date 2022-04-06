a1251c68da842c3ff07015691012422f915f0c67
Rustam Antia <rustam.antia@dida.do>
2022-04-06 13:16:14 +0200

    JAXA downloader for single polygon

--- rs_tools/img_download/jaxa_downloader_for_single_polygon.py
+++ rs_tools/img_download/jaxa_downloader_for_single_polygon.py
@@ -1,26 +1,16 @@
-"""data_version '1804' jaxa_file_name 'S034E152.tar.gz' jaxa_folder_name
-'S035E150/'.
-
-urllib.error.URLError: <urlopen error ftp error: URLError("ftp error: error_perm('550 S029E154.tar.gz: No such file or directory')")>
-
-Downloader mix in that obtains digital elevation model (DEM) data
+"""
+ImgDownloaderForSinglePolygon that downloads digital elevation model (DEM) data
 from jaxa.jp's ALOS data-source.
 
-Background: https://www.eorc.jaxa.jp/ALOS/en/index.htm
-ALOS product description (file-format, etc):
+See here https://www.eorc.jaxa.jp/ALOS/en/index.htm for an overview of the ALOS data.
+A detailed product description for ALOS (file-format, etc) can be found in:
 https://www.eorc.jaxa.jp/ALOS/en/aw3d30/aw3d30v3.2_product_e_e1.0.pdf
 The data is assumed to be stored on the FTP server:
 ftp://ftp.eorc.jaxa.jp/pub/ALOS/ext1/AW3D30/release_vXXXX/
 (port: 46287)
 
-different versions exist
-––– 1804
-––– 1903
-––– 2003
-––– 2012
+There are different versions of the ALOS data: 1804, 1903, 2003, 2012. Only the 1804 version has been tested.
 """
-
-from __future__ import annotations
 
 import logging
 import math
@@ -37,6 +27,7 @@ import numpy as np
 import rasterio as rio
 from shapely.geometry import box
 from shapely.geometry.polygon import Polygon
+from rs_tools.img_download.base_downloader_for_single_polygon import ImgDownloaderForSinglePolygon
 
 from rs_tools.utils.utils import transform_shapely_geometry
 
@@ -47,16 +38,16 @@ JAXA_DATA_VERSIONS = ['1804', '1903', '2003',
                       '2012']  # (attn: only 1804 has been tested so far)
 
 
-class JAXADownloaderMixIn(object):
+class JAXADownloaderForSinglePolygon(ImgDownloaderForSinglePolygon):
     """Download JAXA DEM (digital elevation) data."""
 
-    @property
-    def jaxa_data_version(self):
-        return self._params_dict['jaxa_data_version']
+    # @property
+    # def jaxa_data_version(self):
+    #     return self._params_dict['jaxa_data_version']
 
-    @jaxa_data_version.setter
-    def jaxa_data_version(self, new_jaxa_version: str):
-        self._params_dict['jaxa_data_version'] = new_jaxa_version
+    # @jaxa_data_version.setter
+    # def jaxa_data_version(self, new_jaxa_version: str):
+    #     self._params_dict['jaxa_data_version'] = new_jaxa_version
 
     def _download_imgs_for_polygon_jaxa(
             self,
@@ -69,12 +60,6 @@ class JAXADownloaderMixIn(object):
             **kwargs):
         """Downloads DEM data from jaxa.jp's ftp-server for a given polygon and
         returns dict-structure compatible with the image-polygon-associator.
-
-        Note:
-            - Only operates on a single polygon
-            - Does not collate multiple images currently
-            - Only returns a single exception / error-code
-                (not one per file downloaded)
 
         Warning:
             The downloader has only been tested for the 1804 jaxa_data_version.
@@ -96,7 +81,7 @@ class JAXADownloaderMixIn(object):
                 Defaults if possible to whichever choice you made last time.
             jaxa_download_mode (str): One of 'bboxvertices', 'bboxgrid'.
                 Defaults if possible to whichever choice you made last time.
-            **kwargs (Any): currently ignored
+            **kwargs (Any): other kwargs, ignored.
 
         Returns:
             dict of dicts according to the associator convention
@@ -107,35 +92,13 @@ class JAXADownloaderMixIn(object):
             (download_exception = 'file_not_available_on_JAXA_ftp')
         """
 
-        for jaxa_specific_keyword_arg, value in {
-            ('data_version', data_version),
-            ('download_mode', download_mode),
-        }:
-
-            if value is None:
-                try:
-                    # Use saved value
-                    value = getattr(self, f"jaxa_{jaxa_specific_keyword_arg}")
-                except (AttributeError, KeyError):
-                    raise ValueError(
-                        f"Need to set {jaxa_specific_keyword_arg} keyword argument for the jaxa downloader."
-                    )
-            else:
-                # Remember value
-                setattr(self, f"jaxa_{jaxa_specific_keyword_arg}", value)
-
         if data_version not in JAXA_DATA_VERSIONS:
             raise ValueError(
                 f"Unknown data_version {data_version}. Should be one of {', '.join(JAXA_DATA_VERSIONS)}"
             )
 
         jaxa_file_and_folder_names = set()
-
         if download_mode == 'bboxvertices':
-
-            # obtain all files that intersect with the vertices of the bounding box of the polygon
-            # ATTENTION: if polygon extends beyond 2 files in width or height this might miss files in between
-            # the ones containing the outer points
 
             for (x, y) in polygon_geometry.envelope.exterior.coords:
 
@@ -172,35 +135,35 @@ class JAXADownloaderMixIn(object):
         list_img_info_dicts = [
         ]  # to collect information per downloaded file for associator
 
-        # download_exception = str(None)
-        # have_img_downloaded = False
-
         for jaxa_file_name, jaxa_folder_name in jaxa_file_and_folder_names:
 
+            # Skip download if file has already been downloaded ...
             if jaxa_file_name[:
                               -7] + '_DSM.tif' in previously_downloaded_imgs_set:
                 # in this case skip download, don't store in list_img_info_dicts
-                log.info('Skipping download for image ' + jaxa_file_name)
+                log.info('Skipping download for image %s', jaxa_file_name)
                 continue
+            # ... else, download.
             else:
-                # downloading from jaxa's FTP
-                log.info(   f'Downloading from ftp.eorc.jaxa.jp (v{data_version}) for polygon {polygon_name} ' \
-                            f'to: {os.path.join(download_dir, jaxa_file_name)}')
+                log.info(
+                    'Downloading from ftp.eorc.jaxa.jp (v%s) for polygon %s',
+                    data_version, polygon_name)
+                log.info('Downloading to: %s',
+                         os.path.join(download_dir, jaxa_file_name))
                 try:
                     with closing(request.urlopen('ftp://ftp.eorc.jaxa.jp/pub/ALOS/ext1/AW3D30/release_v' + data_version + '/' \
                                                     + jaxa_folder_name + jaxa_file_name)) as remote_source:
                         with open(os.path.join(download_dir, jaxa_file_name),
                                   'wb') as local_file:
                             shutil.copyfileobj(remote_source, local_file)
-                except Exception as e:
+                except Exception as exc:
                     log.warning(
-                        f'File {jaxa_file_name} in folder {jaxa_folder_name} could not be found on JAXA ftp or could not be opened: {e.args}'
-                    )
+                        'File %s in folder %s could not be found on JAXA ftp or could not be opened: %s',
+                        jaxa_file_name, jaxa_folder_name, exc.args)
                     # continue
-                    # download_exception = 'file_not_available_on_JAXA_ftp'
 
                 else:
-                    # extracting .tar file and deleting it afterwards
+                    # Extract downloaded .tar file ...
                     tar = tarfile.open(
                         os.path.join(download_dir, jaxa_file_name), "r:gz")
                     tar.extractall(path=download_dir,
@@ -208,85 +171,26 @@ class JAXADownloaderMixIn(object):
                                        tar.getmembers()[1]
                                    ])  # extract only DSM.tif from archive
                     tar.close()
-                    os.remove(os.path.join(download_dir,
-                                           jaxa_file_name))  # delete .tar file
-                    # move needed DEM-file outside of unzipped folder (existing versions are overwritten)
+                    # ... and after extracting delete it.
+                    os.remove(os.path.join(download_dir, jaxa_file_name))
+
                     shutil.move(
-                        str(
-                            Path(download_dir) / jaxa_file_name[:-7] /
+                        str(download_dir / jaxa_file_name[:-7] /
                             (jaxa_file_name[:-7] + '_AVE_DSM.tif')),
-                        str(
-                            Path(download_dir) /
-                            (jaxa_file_name[:-7] + '_DSM.tif')))
-                    os.rmdir(
-                        os.path.join(Path(download_dir), jaxa_file_name[:-7])
-                    )  # remove folder, only works when it is empty
+                        str(download_dir / (jaxa_file_name[:-7] + '_DSM.tif')))
+                    shutil.rmtree(download_dir / jaxa_file_name[:-7],
+                                  ignore_errors=True)
 
-                    # -- skipped -- only relevant when not iterating over polygons -------
-                    # check if polygon is within boundaries of DEM file
-                    # if download_exception == str(None):     # only if DEM was obtained successfully
-                    #     with rio.open(str(Path(download_dir) / (jaxa_file_name[:-7] + '_AVE_DSM.tif'))) as src:
-                    #         img_bounding_rectangle = box(*src.bounds)
-                    #         if img_bounding_rectangle.contains(polygon_geometry) == False:
-                    #             log.warning('Polygon ' + polygon_name + ' not fully contained in downloaded DEM. Processing errors might occur.')
-                    #             log.warning('Polygon ' + polygon_name + ' bounds: ' + str(polygon_geometry.envelope.exterior))
-                    #             log.warning('DEM ' + jaxa_file_name + ' bounds: ' + str(img_bounding_rectangle))
-                    #             download_exception = 'polygon_outside_file_bounds'
-
-                    # have_img_downloaded = True
-                    dateTimeObj = datetime.now()
-                    list_img_info_dicts.append({
-                        'img_name':
-                        jaxa_file_name[:-7] +
-                        '_DSM.tif',  # TODO: check relevance of _AVE_ file-name component, consider to rename/remove
-                        'img_processed?':
-                        False,
-                        'timestamp':
-                        dateTimeObj.strftime("%Y-%m-%d-%H:%M:%S")
-                    })
+                    date_time_now = datetime.now().strftime(
+                        "%Y-%m-%d-%H:%M:%S")
+                    img_info_dict = {
+                        'img_name': jaxa_file_name[:-7] + '_DSM.tif',
+                        'img_processed?': False,
+                        'timestamp': date_time_now,
+                    }
+                    list_img_info_dicts.append(img_info_dict)
 
         return {'list_img_info_dicts': list_img_info_dicts}
-
-    def _process_downloaded_img_file_jaxa(self, img_name: str,
-                                          in_dir: Union[Path, str],
-                                          out_dir: Union[Path, str],
-                                          convert_to_crs_epsg: int, **kwargs):
-        """provides required postprocessing for downloaded jaxa DEM. Currently
-        only copying from in_dir to out_dir is performed.
-
-        Note:
-        - Future update could include interpolation of DEM data to increase resolution for
-        better visualization
-
-        Args:
-        all arguments are provided in the contex of the higher-level function ImgPolygonAssociator
-        which calls this function
-        """
-
-        # obtain target coordinate-ref-system code from source file:
-        geotif_filename = Path(in_dir) / img_name
-        with rio.open(geotif_filename) as src:
-            orig_crs_epsg_code = src.crs.to_epsg()
-            img_bounding_rectangle = box(*src.bounds)
-
-        # move file
-        shutil.move(Path(in_dir) / img_name, Path(out_dir) / img_name)
-
-        # setting relevant assoc properties
-        img_info_dict = {
-            'orig_crs_epsg_code':
-            orig_crs_epsg_code,  # alternatively directly use 4326 for WGS84 (used for jaxa)
-            'img_name':
-            img_name,
-            'img_processed?':
-            True,
-            'geometry':
-            transform_shapely_geometry(img_bounding_rectangle,
-                                       orig_crs_epsg_code, 4326)
-        }
-
-        log.info('Successfully downloaded & processed file ' + img_name)
-        return img_info_dict
 
     def _obtain_jaxa_index(self,
                            x: Optional[float] = None,
