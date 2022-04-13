34ea8abf29eb8af16b2646705e3bc9624300cf9d
Rustam Antia <rustam.antia@dida.do>
2022-04-13 17:19:10 +0200

    standardized class name (CreateDSCut.. -> DSCutter...). module docstring. fix private method name

--- rs_tools/cut/cut_every_img_to_grid.py
+++ rs_tools/cut/cut_every_img_to_grid.py
@@ -1,8 +1,9 @@
-"""Mixin to cut datasets of GeoTiffs (or update previously cut datasets) by
-cutting each image in the source dataset to a grid of images."""
+"""Dataset cutter that cuts images in the source dataset to a grid of images"""
 
+from typing import List, Optional
 import logging
 
+from pydantic import Field
 from rs_tools.cut.cut_iter_over_imgs import DSCutterIterOverImgs
 from rs_tools.cut.type_aliases import ImgSize
 from rs_tools.cut.img_filter_predicates import AlwaysTrue, ImgFilterPredicate
@@ -11,10 +12,16 @@ from rs_tools.cut.single_img_cutter_grid import ImgToGridCutter
 logger = logging.getLogger(__name__)
 
 
-class CreateDSCutEveryImgToGrid(DSCutterIterOverImgs):
+class DSCutterEveryImgToGrid(DSCutterIterOverImgs):
     """Dataset cutter that cuts images in the source dataset to a grid of images"""
 
-    new_img_size: ImgSize
+    new_img_size: Field(
+        ImgSize,
+        description=
+        "Size of cutouts. Passed to ImgToGridCutter during __init__ only.")
+
+    class Config:
+        arbitrary_types_allowed = True
 
     def __init__(
         self,
@@ -31,6 +38,6 @@ class CreateDSCutEveryImgToGrid(DSCutterIterOverImgs):
             new_img_size=new_img_size,
         )
 
-    def _after_cutting(self):
+    def _after_creating_or_updating(self):
         self.target_assoc._params_dict["img_size"] = self.new_img_size
         self.target_assoc.save()
