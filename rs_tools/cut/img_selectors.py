9dee4414eca56d32dec2988d9e3616ccbc8cf273
Rustam Antia <rustam.antia@gmail.com>
2022-04-13 18:12:46 +0200

    cut_imgs arg in img_selector

--- rs_tools/cut/img_selectors.py
+++ rs_tools/cut/img_selectors.py
@@ -1,20 +1,20 @@
-"""Callable classes for selecting a sublist from a list of images used by
-cutting functions."""
+"""
+TODO: remove reference to _update_from_source_dataset_dict!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-from __future__ import annotations
+Callable classes for selecting a sublist from a list of images.
+Used by cutting functions.
+"""
 
 import random
 from abc import abstractmethod
 from collections.abc import Callable
-from typing import TYPE_CHECKING, Any, List, Union
+from typing import Any, Dict, List, Union
 
-from geopandas import GeoDataFrame
-
-if TYPE_CHECKING:
-    from rs_tools import ImgPolygonAssociator
+from pydantic import BaseModel
+from rs_tools import ImgPolygonAssociator
 
 
-class ImgSelector(Callable):
+class ImgSelector(Callable, BaseModel):
     """Abstract base class for selecting from a list of images. Subclasses are
     used by create_or_update_dataset_from_iter_over_polygons.
 
@@ -23,10 +23,15 @@ class ImgSelector(Callable):
     """
 
     @abstractmethod
-    def __call__(self, img_names_list: List[str],
-                 target_assoc: ImgPolygonAssociator, new_imgs_dict: dict,
-                 source_assoc: ImgPolygonAssociator,
-                 **kwargs: Any) -> List[str]:
+    def __call__(
+        self,
+        img_names_list: List[str],
+        target_assoc: ImgPolygonAssociator,
+        new_imgs_dict: dict,
+        source_assoc: ImgPolygonAssociator,
+        cut_imgs: Dict[str, List[str]],
+        **kwargs: Any,
+    ) -> List[str]:
         """Override to subclass. If img_names_list is empty an empty list
         should be returned.
 
@@ -45,28 +50,30 @@ class ImgSelector(Callable):
         Note:
             It should be possible for the returned sublist to depend on all the information in the source and target associators. The ImgSelector used by the cutting function create_or_update_tif_dataset_from_iter_over_polygons in rs_tools.cut.cut_iter_over_polygons. This function does not concatenate the information about the new images that have been cut to the target_assoc.imgs_df until after all polygons have been iterated over. We want to use the polygon filter predicate _during_ this iteration, so we allow the call function to also depend on a new_imgs_dict argument which contains the information about the new images that have been cut. Unlike the target_assoc.imgs_df, the target_assoc.polygons_df and graph are updated during the iteration. One should thus think of the target_assoc and new_imgs_dict arguments together as the actual the target associator argument.
         """
-        pass
 
 
 class RandomImgSelector(ImgSelector):
     """ImgSelector that randomly selects randomly from a list of images."""
 
-    def __init__(self, target_img_count: int = 1):
+    target_img_count: int = 1
 
-        super().__init__()
-        self.target_img_count = target_img_count
-
-    def __call__(self, polygon_name: Union[str,
-                                           int], img_names_list: List[str],
-                 target_assoc: ImgPolygonAssociator, new_imgs_dict: dict,
-                 source_assoc: ImgPolygonAssociator) -> List[str]:
+    def __call__(
+        self,
+        polygon_name: Union[str, int],
+        img_names_list: List[str],
+        target_assoc: ImgPolygonAssociator,
+        new_imgs_dict: dict,
+        source_assoc: ImgPolygonAssociator,
+        cut_imgs: Dict[str, List[str]],
+        **kwargs: Any,
+    ) -> List[str]:
         """
         Select target_img_count - #{img_count of polygon in target_assoc} images (or if not possible less) from img_names_list.
         """
 
         target_num_imgs_to_sample = self.target_img_count \
             - len(target_assoc.imgs_containing_polygon(polygon_name)) \
-            - len(target_assoc._update_from_source_dataset_dict['cut_imgs'][polygon_name])
+            - len(cut_imgs[polygon_name])
 
         # can only sample a non-negative number of images
         target_num_imgs_to_sample = max(0, target_num_imgs_to_sample)
