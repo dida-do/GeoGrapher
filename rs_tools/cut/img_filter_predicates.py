b3f7a08f6b275225b8f7924f0f7b8a673314e645
Rustam Antia <rustam.antia@gmail.com>
2022-04-13 17:21:23 +0200

    function to wrap function as row series predicate. formatting. fix imports

--- rs_tools/cut/img_filter_predicates.py
+++ rs_tools/cut/img_filter_predicates.py
@@ -1,19 +1,18 @@
 """Abstract base class for predicates used to filter images in cutting
 functions."""
-from __future__ import annotations
-
-from abc import abstractmethod
+from pathlib import Path
+from abc import abstractmethod, ABC
 from collections.abc import Callable
-from typing import TYPE_CHECKING, Any, Callable, Union
+from typing import Union
 
 from geopandas import GeoSeries
 from pandas import Series
+from pydantic import BaseModel
 
-if TYPE_CHECKING:
-    from rs_tools import ImgPolygonAssociator
+from rs_tools import ImgPolygonAssociator
 
 
-class ImgFilterPredicate(Callable):
+class ImgFilterPredicate(ABC, Callable, BaseModel):
     """Abstract base class for predicates used to filter images in cutting
     functions.
 
@@ -22,37 +21,48 @@ class ImgFilterPredicate(Callable):
     """
 
     @abstractmethod
-    def __call__(self, img_name: str, target_assoc: ImgPolygonAssociator,
-                 new_img_dict: dict,
-                 source_assoc: ImgPolygonAssociator) -> bool:
+    def __call__(
+        self,
+        img_name: str,
+        target_assoc: ImgPolygonAssociator,
+        new_img_dict: dict,
+        source_assoc: ImgPolygonAssociator,
+    ) -> bool:
         """
         Args:
             img_name (str): img identifier
-            target_assoc (ImgPolygonAssociator): associator of target dataset. 
+            target_assoc (ImgPolygonAssociator): associator of target dataset.
             new_imgs_dict (dict): dict with keys index or column names of target_assoc.imgs_df and values lists of entries correspondong to images 
-            source_assoc (ImgPolygonAssociator): associator of source dataset that new images are being cut out from 
+            source_assoc (ImgPolygonAssociator): associator of source dataset that new images are being cut out from
 
         Returns:
             bool: True should mean image is to be kept, False that it is to be filtered out
 
-        Note: 
-            The new_imgs_dict should be viewed as part of the target associator. See polygon_filter_predicates.py for an explanation. 
+        Note:
+            The new_imgs_dict should be viewed as part of the target associator. See polygon_filter_predicates.py for an explanation.
         """
         raise NotImplementedError
 
+    def save(self, save_dir: Path) -> None:
+        """Save the predicate to a given directory."""
+        save_dir.mkdir(exist_ok=True)
+        with open(save_dir, 'w') as f:
+            f.write(self.json(indent=2))
+
 
 class AlwaysTrue(ImgFilterPredicate):
-    """Default polygon filter predicate that always returns True.
+    """Default image filter predicate that always returns True.
 
     Used when filtering is not desired.
     """
 
-    def __init__(self) -> None:
-        super().__init__()
-
-    def __call__(self, img_name: str, target_assoc: ImgPolygonAssociator,
-                 new_img_dict: dict,
-                 source_assoc: ImgPolygonAssociator) -> bool:
+    def __call__(
+        self,
+        img_name: str,
+        target_assoc: ImgPolygonAssociator,
+        new_img_dict: dict,
+        source_assoc: ImgPolygonAssociator,
+    ) -> bool:
         """Return True."""
         return True
 
@@ -60,6 +70,8 @@ class AlwaysTrue(ImgFilterPredicate):
 class ImgFilterRowCondition(ImgFilterPredicate):
     """Simple ImgFilter that applies a given predicate to the row in
     source_assoc.imgs_df corresponding to the image name in question."""
+
+    row_series_predicate: RowSeriesPredicate
 
     def __init__(
         self, row_series_predicate: Callable[[Union[GeoSeries, Series]],
@@ -72,9 +84,13 @@ class ImgFilterRowCondition(ImgFilterPredicate):
         super().__init__()
         self.row_series_predicate = row_series_predicate
 
-    def __call__(self, img_name: str, target_assoc: ImgPolygonAssociator,
-                 new_img_dict: dict,
-                 source_assoc: ImgPolygonAssociator) -> bool:
+    def __call__(
+        self,
+        img_name: str,
+        target_assoc: ImgPolygonAssociator,
+        new_img_dict: dict,
+        source_assoc: ImgPolygonAssociator,
+    ) -> bool:
         """Apply self.row_series_predicate to source_assoc.imgs_df[img_name]
 
         Args:
@@ -93,3 +109,21 @@ class ImgFilterRowCondition(ImgFilterPredicate):
         answer = self.row_series_predicate(row_series)
 
         return answer
+
+
+class RowSeriesPredicate(ABC, BaseModel):
+
+    @abstractmethod
+    def __call__(*args, **kwargs):
+        pass
+
+
+def wrap_function_as_RowSeriesPredicate(
+        fun: Callable[[Union[GeoSeries, Series]], bool]) -> RowSeriesPredicate:
+
+    class WrappedAsRowSeriesPredicate(RowSeriesPredicate):
+
+        def __call__(*args, **kwargs):
+            return fun(*args, **kwargs)
+
+    return WrappedAsRowSeriesPredicate
