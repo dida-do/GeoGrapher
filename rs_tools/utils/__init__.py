dcda703e4c9c22d620e0df23a13abc1e93c2e3ac
Rustam Antia <rustam.antia@dida.do>
2022-04-23 11:30:15 +0200

    renaming of utils files

--- rs_tools/utils/__init__.py
+++ rs_tools/utils/__init__.py
@@ -1,11 +1,10 @@
 """Utils for handling remote sensing datasets.
 
-convert_assoc_dataset_tif2npy converts a dataset of GeoTiffs to .npys.
+convert_connector_dataset_tif2npy converts a dataset of GeoTiffs to .npys.
 img_data_from_tif_dir generates an img_data GeoDataFrame from a directory
 of GeoTiffs.
 """
 
-from rs_tools.utils.img_data_from_tif_dir import (
+from rs_tools.utils.raster_imgs_from_tif_dir import (
     default_read_in_img_for_img_df_function, img_data_from_imgs_dir)
 from rs_tools.utils.utils import deepcopy_gdf, transform_shapely_geometry
-
