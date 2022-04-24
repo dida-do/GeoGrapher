a629dc90116d31413a41662b5df34d22e7564902
Rustam Antia <rustam.antia@dida.do>
2022-04-24 21:21:43 +0200

    renaming

--- rs_tools/global_constants.py
+++ rs_tools/global_constants.py
@@ -3,5 +3,5 @@ from pathlib import Path
 STANDARD_CRS_EPSG_CODE = 4326  # WGS84
 DATA_DIR_SUBDIRS = [Path("images"),
                     Path("labels")]  # for sentinel-2, also Path("safe_files")
-RASTER_FEATURES_INDEX_NAME = "img_name"
-VECTOR_DATA_INDEX_NAME = "geom_name"
+RASTER_IMGS_INDEX_NAME = "img_name"
+VECTOR_FEATURES_INDEX_NAME = "feature_name"
