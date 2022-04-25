29ec0619ebd83bae9da4530c4e378adecbff0830
Rustam Antia <rustam.antia@dida.do>
2022-04-25 16:25:42 +0200

    fix import in init

--- rs_tools/cutters/__init__.py
+++ rs_tools/cutters/__init__.py
@@ -1,4 +1,4 @@
 from rs_tools.cutters.cut_every_img_to_grid import DSCutterEveryImgToGrid
-from rs_tools.cutters.cut_imgs_around_every_geom import DSCutterImgsAroundEveryFeature
+from rs_tools.cutters.cut_imgs_around_every_feature import DSCutterImgsAroundEveryFeature
 from rs_tools.cutters.cut_iter_over_imgs import DSCutterIterOverImgs
-from rs_tools.cutters.cut_iter_over_geoms import DSCutterIterOverFeatures
+from rs_tools.cutters.cut_iter_over_feature import DSCutterIterOverFeatures
