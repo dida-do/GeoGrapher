7c0b4f71ffdd02d7c3f131ba1baca5f4da24ba87
Rustam Antia <rustam.antia@dida.do>
2022-05-13 11:58:59 +0200

    complete renaming

--- rs_tools/cutters/__init__.py
+++ rs_tools/cutters/__init__.py
@@ -1,4 +1,4 @@
 from rs_tools.cutters.cut_every_img_to_grid import DSCutterEveryImgToGrid
 from rs_tools.cutters.cut_imgs_around_every_feature import DSCutterImgsAroundEveryFeature
 from rs_tools.cutters.cut_iter_over_imgs import DSCutterIterOverImgs
-from rs_tools.cutters.cut_iter_over_feature import DSCutterIterOverFeatures
+from rs_tools.cutters.cut_iter_over_features import DSCutterIterOverFeatures
