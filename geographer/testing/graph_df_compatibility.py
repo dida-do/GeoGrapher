41b6951cd6c1b67ac8b386b7cf7f99f041fb2a62
Rustam Antia <rustam.antia@dida.do>
2022-05-27 21:41:41 +0200

    clean up graph_df_compatibility

--- geographer/testing/graph_df_compatibility.py
+++ geographer/testing/graph_df_compatibility.py
@@ -1,4 +1,4 @@
-"""Functions useful for testing or debugging."""
+"""Test compatibility of raster_imgs, vector_features, and graph"""
 
 import logging
 
@@ -8,35 +8,35 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 
-def test_graph_vertices_counts(assoc):
-    """Test associator invariant.
+def test_graph_vertices_counts(connector):
+    """Test connector invariant.
 
     Tests whether the set of vertices of the graph corresponds with the
-    images and polygons in the associator and whether the number of
+    images and polygons in the connector and whether the number of
     edges leaving the polygon nodes corresponding to imagges fully
-    containing the polygon are equal to the values in the polygons_df
+    containing the polygon are equal to the values in the vector_features
     'img_count' column.
     """
 
-    img_vertices_not_in_raster_imgs = set(assoc._graph.vertices('imgs')) - set(
-        assoc.raster_imgs.index)
-    imgs_in_raster_imgs_not_in_graph = set(assoc.raster_imgs.index) - set(
-        assoc._graph.vertices('imgs'))
-    polygon_vertices_not_in_polygons_df = set(
-        assoc._graph.vertices('polygons')) - set(assoc.polygons_df.index)
-    polygons_in_polygons_df_not_in_graph = set(assoc.polygons_df.index) - set(
-        assoc._graph.vertices('polygons'))
+    img_vertices_not_in_raster_imgs = set(connector._graph.vertices('imgs')) - set(
+        connector.raster_imgs.index)
+    imgs_in_raster_imgs_not_in_graph = set(connector.raster_imgs.index) - set(
+        connector._graph.vertices('imgs'))
+    polygon_vertices_not_in_vector_features = set(
+        connector._graph.vertices('polygons')) - set(connector.vector_features.index)
+    polygons_in_vector_features_not_in_graph = set(connector.vector_features.index) - set(
+        connector._graph.vertices('polygons'))
 
     set_descriptions_and_differences = \
         zip(
-            [('image', 'in the associator\'s graph not in raster_imgs'),
-                ('image', 'in the associator\'s raster_imgs not in the graph'),
-                ('polygon', 'in the associator\'s graph not in polygons_df'),
-                ('polygon', 'in the associator\'s polygons_df not in graph')], \
+            [('image', 'in the connector\'s graph not in raster_imgs'),
+                ('image', 'in the connector\'s raster_imgs not in the graph'),
+                ('polygon', 'in the connector\'s graph not in vector_features'),
+                ('polygon', 'in the connector\'s vector_features not in graph')], \
             [img_vertices_not_in_raster_imgs,
                 imgs_in_raster_imgs_not_in_graph,
-                polygon_vertices_not_in_polygons_df,
-                polygons_in_polygons_df_not_in_graph])
+                polygon_vertices_not_in_vector_features,
+                polygons_in_vector_features_not_in_graph])
 
     answer = True
 
@@ -57,16 +57,16 @@ def test_graph_vertices_counts(assoc):
             )
 
     # Now, check whether img_count column agrees with results of img_containing_polygon for each polygon
-    img_count_edges = assoc.polygons_df.apply(
-        lambda row: len(assoc.imgs_containing_polygon(row.name)), axis=1)
+    img_count_edges = connector.vector_features.apply(
+        lambda row: len(connector.imgs_containing_polygon(row.name)), axis=1)
     img_count_edges.rename("img_count_edges", inplace=True)
 
-    counts_correct = assoc.polygons_df['img_count'] == img_count_edges
+    counts_correct = connector.vector_features['img_count'] == img_count_edges
 
     if not counts_correct.all():
 
         return_df = pd.concat(
-            [assoc.polygons_df['img_count'], img_count_edges], axis=1)
+            [connector.vector_features['img_count'], img_count_edges], axis=1)
         return_df = return_df.loc[~counts_correct]
 
         logger.error(
