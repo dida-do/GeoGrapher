619e18361ba36c90481e74fb0ea7e523a6f3f0c9
Rustam Antia <rustam.antia@dida.do>
2022-05-27 12:12:08 +0200

    add (manual) test for downloading JAXA data

---
+++ tests/download_jaxa_test_manually.py
@@ -0,0 +1,77 @@
+"""
+Run by hand to test downloading JAXA data. Intentionally not discoverable
+by pytest: Downloading JAXA images is slightly slow.
+"""
+
+
+from pathlib import Path
+import shutil
+import git
+import geopandas as gpd
+from pydantic import BaseModel
+
+from geographer import Connector
+from geographer.downloaders.downloader_for_features import ImgDownloaderForVectorFeatures
+from geographer.downloaders.jaxa_downloader_for_single_feature import JAXADownloaderForSingleVectorFeature
+from geographer.downloaders.jaxa_download_processor import JAXADownloadProcessor
+
+def test_jaxa_download():
+    """Test downloading JAXA data"""
+
+    """
+    Create connector containing vector_features but no raster images
+    """
+    # get repo working tree directory
+    repo = git.Repo('.', search_parent_directories=True)
+    repo_root = Path(repo.working_tree_dir)
+
+    download_test_data_dir = repo_root / "tests/data/temp/download_jaxa_test"
+
+    vector_features = gpd.read_file(repo_root / 'tests/geographer_download_test.geojson', driver='GeoJSON')
+    vector_features.set_index("name", inplace=True)
+
+    connector = Connector.from_scratch(data_dir=download_test_data_dir ,task_feature_classes=['object'])
+    connector.add_to_vector_features(vector_features)
+
+    """
+    Test ImgDownloaderForVectorFeatures for Sentinel-2 data
+    """
+    jaxa_download_processor = JAXADownloadProcessor()
+    jaxa_downloader_for_single_feature = JAXADownloaderForSingleVectorFeature()
+    jaxa_downloader = ImgDownloaderForVectorFeatures(
+        download_dir=download_test_data_dir / "download",
+        downloader_for_single_feature=jaxa_downloader_for_single_feature,
+        download_processor=jaxa_download_processor,
+        kwarg_defaults={
+            "data_version": "1804",
+            "download_mode": "bboxvertices",
+        }
+    )
+
+    """
+    Download Sentinel-2 images
+    """
+    jaxa_downloader.download(connector=connector)
+    # The vector_features contain
+    #     - 2 objects in Berlin (Reichstag and Brandenburg gate) that are very close to each other
+    #     - 2 objects in Lisbon (Castelo de Sao Jorge and Praca Do Comercio) that are very close to each other.
+    # Thus the jaxa_downloader should have downloaded two images, one for Berlin and one for Lisbon, each containing two objects.
+
+    # Berlin
+    assert len(connector.imgs_containing_vector_feature("berlin_reichstag")) == 1
+    assert len(connector.imgs_containing_vector_feature("berlin_brandenburg_gate")) == 1
+    assert connector.imgs_containing_vector_feature("berlin_reichstag") == connector.imgs_containing_vector_feature("berlin_brandenburg_gate")
+
+    # Lisbon
+    assert len(connector.imgs_containing_vector_feature("lisbon_castelo_de_sao_jorge")) == 1
+    assert len(connector.imgs_containing_vector_feature("lisbon_praca_do_comercio")) == 1
+    assert connector.imgs_containing_vector_feature("lisbon_castelo_de_sao_jorge") == connector.imgs_containing_vector_feature("lisbon_praca_do_comercio")
+
+    """
+    Clean up: delete downloads
+    """
+    shutil.rmtree(download_test_data_dir)
+
+
+if __name__ == "__main__":
+    test_jaxa_download()
