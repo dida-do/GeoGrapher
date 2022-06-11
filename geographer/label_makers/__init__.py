79d014d504a5f445ccf44999f97dc56aa32fa066
Rustam Antia <rustam.antia@dida.do>
2022-06-11 10:16:33 +0200

    debug and add basic test for segmentation label makers

---
+++ geographer/label_makers/__init__.py
@@ -0,0 +1,2 @@
+from geographer.label_makers.seg_label_maker_soft_categorical import SegLabelMakerSoftCategorical
+from geographer.label_makers.seg_label_maker_categorical import SegLabelMakerCategorical
