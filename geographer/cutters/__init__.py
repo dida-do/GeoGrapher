dd54f4623492d0a202462b0ba5514b63a1b1583b
Rustam Antia <rustam.antia@dida.do>
2022-06-11 10:23:59 +0200

    replaced DSCutter classes for cutting around images and every img to grid by functions (consistency with loading BaseModels)

--- geographer/cutters/__init__.py
+++ geographer/cutters/__init__.py
@@ -1,4 +1,4 @@
-from geographer.cutters.cut_every_img_to_grid import DSCutterEveryImgToGrid
-from geographer.cutters.cut_imgs_around_every_feature import DSCutterImgsAroundEveryFeature
+# from geographer.cutters.cut_every_img_to_grid import DSCutterEveryImgToGrid
+# from geographer.cutters.cut_imgs_around_every_feature import DSCutterImgsAroundEveryFeature
 from geographer.cutters.cut_iter_over_imgs import DSCutterIterOverImgs
 from geographer.cutters.cut_iter_over_features import DSCutterIterOverFeatures
