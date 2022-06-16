616fb6e4e24003ffbb1e899e9e42e6b9dd6428af
Rustam Antia <rustam.antia@dida.do>
2022-06-16 22:56:51 +0200

    optional arg to select images to create dummy image data for

--- tests/utils.py
+++ tests/utils.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 import shutil
-from typing import Union
+from typing import List, Optional, Union
 import git
 import numpy as np
 import rasterio as rio
+from tqdm.auto import tqdm
 from geographer import Connector
 from geographer.utils.utils import transform_shapely_geometry
 
@@ -14,12 +15,23 @@ def get_test_dir():
     repo = git.Repo('.', search_parent_directories=True)
     return Path(repo.working_tree_dir) / "tests/data"
 
-def create_dummy_imgs(data_dir: Union[Path, str], img_size: int) -> None:
+def create_dummy_imgs(
+    data_dir: Union[Path, str],
+    img_size: int,
+    img_names: Optional[List[str]]=None
+    ) -> None:
     """Create dummy images for a dataset from the connector's raster_imgs geodataframe"""
     connector = Connector.from_data_dir(data_dir)
     connector.images_dir.mkdir(parents=True, exist_ok=True)
-    for img_name, bbox_geom, epsg_code in connector.raster_imgs[["geometry", "orig_crs_epsg_code"]].itertuples():
-        img_array = np.stack(np.ones((img_size, img_size), dtype=np.uint8) * n for n in range(3))
+
+    if img_names is None:
+        img_names = connector.raster_imgs.index.tolist()
+
+    for img_name, bbox_geom, epsg_code in tqdm(
+        list(connector.raster_imgs[["geometry", "orig_crs_epsg_code"]].loc[img_names].itertuples()),
+        desc="Creating dummy images"):
+
+        img_array = np.stack([np.ones((img_size, img_size), dtype=np.uint8) * n for n in range(3)])
         bbox_geom_in_img_crs = transform_shapely_geometry(bbox_geom, from_epsg=connector.crs_epsg_code, to_epsg=epsg_code)
         transform = rio.transform.from_bounds(*bbox_geom_in_img_crs.bounds, img_size, img_size)
 
