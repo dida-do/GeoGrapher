6df3905475cee378ddb0a02bd4d04cf1ee0d096b
Rustam Antia <rustam.antia@dida.do>
2022-09-10 19:40:18 +0200

    add noqa

--- geographer/cutters/type_aliases.py
+++ geographer/cutters/type_aliases.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from typing import Optional, Tuple, Union
+from typing import Optional, Tuple, Union  # noqa: I001,I005
 
 # Tuple instead of tuple because pydantic needs old-style
 # type declarations for python 3.8
