af8e508a20e40e2ed87b13c2ba497eefc87c0409
rustamdantia <57261658+rustamdantia@users.noreply.github.com>
2022-09-28 17:27:26 +0200

    First two example/tutorial notebooks (#59)
    Add two example/tutorial notebooks

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,6 +38,7 @@ extensions = [
     'sphinx.ext.autosummary',
     'sphinx.ext.viewcode',
     'sphinx.ext.todo',
+    'nbsphinx',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
