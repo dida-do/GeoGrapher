132ab2bc3d85f1ecf0c989dc8b5455750f3411b7
Rustam Antia <rustam.antia@dida.do>
2025-03-17 15:18:33 +0100

    Update version in docs/source/conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "Open Source TBD"
 author = "Rustam Antia"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.0"
+release = "1.0.0"
 
 
 # -- General configuration ---------------------------------------------------
